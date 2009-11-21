a8a48f508710eaa94e54ef5c108f8af76e28c9a9
James Croft <James.R.Croft@googlemail.com>
2009-11-21 12:24:46 -0500

    unpacked testing gems

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,5 +28,5 @@ config.action_mailer.delivery_method = :test
 # config.active_record.schema_format = :sql
 
 config.gem 'rspec'
-config.gem 'rspce-rails'
+config.gem 'rspec-rails'
 config.gem 'cucumber'
