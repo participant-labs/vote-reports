d51b8b4bcbf2cdaa51a7b38f6c02a1aac4a4c976
James Croft <James.R.Croft@googlemail.com>
2009-11-21 12:04:24 -0500

    added and unpacked authlogic

--- config/environment.rb
+++ config/environment.rb
@@ -16,7 +16,7 @@ Rails::Initializer.run do |config|
 
   # Specify gems that this application depends on and have them installed with rake gems:install
   # config.gem "bj"
-  # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
+  config.gem "authlogic", :version => '>=2.1.3'
   # config.gem "sqlite3-ruby", :lib => "sqlite3"
   # config.gem "aws-s3", :lib => "aws/s3"
 
