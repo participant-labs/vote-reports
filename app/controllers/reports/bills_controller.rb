70882f8400b2850c16f2d55c23263be2d4d6c1ec
Ben Woosley <ben.woosley@gmail.com>
2009-11-22 12:44:19 -0500

    Fix a bad call to Bill.fetch_by_query

--- app/controllers/reports/bills_controller.rb
+++ app/controllers/reports/bills_controller.rb
@@ -1,6 +1,6 @@
 class Reports::BillsController < ApplicationController
   def new
     @report = current_user.reports.find(params[:report_id])
-    @bills = Bill.find_by_query(params[:q])
+    @bills = Bill.fetch_by_query(params[:q])
   end
 end
--- spec/models/bill_spec.rb
+++ spec/models/bill_spec.rb
@@ -1,24 +1,24 @@
 require 'spec_helper'
 
 describe Bill do
-  describe ".find_by_query" do
+  describe ".fetch_by_query" do
     context "when there are no matches" do
       it "should return an empty array" do
-        Bill.find_by_query("smelly roses").should == []
+        Bill.fetch_by_query("smelly roses").should == []
       end
     end
 
     context "when there are matches" do
       it "should return bills with titles matching the query" do
-        bills = Bill.find_by_query("Patriot Act")
+        bills = Bill.fetch_by_query("Patriot Act")
         bills.map(&:title).should include("USA PATRIOT Reauthorization Act of 2009")
       end
     end
   end
 
-  describe ".find" do
+  describe ".fetch" do
     it "should return bill with the identity in question" do
-      bills = Bill.find("111-h3548")
+      bills = Bill.fetch("111-h3548")
       bills.title.should == "Worker, Homeownership, and Business Assistance Act of 2009"
     end
   end
