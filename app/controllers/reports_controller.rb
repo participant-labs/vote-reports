69a172766edbc4739f892a3d2a392c9a364a2c20
Ben Woosley <ben.woosley@gmail.com>
2009-11-22 17:17:11 -0500

    Move around controllers to better scope reports and make the create report sidebar link work

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,6 +1,15 @@
-class ReportsController < InheritedResources::Base
-  actions :all
-  belongs_to :user, :optional => true
-  before_filter :login_required, :except => [:index, :show]
+class ReportsController < ApplicationController
+  before_filter :login_required
 
+  def new
+    redirect_to new_user_report_path(current_user)
+  end
+
+  def show
+    redirect_to user_report_path(current_user, Report.find(params[:id]))
+  end
+
+  def edit
+    redirect_to edit_user_report_path(current_user, Report.find(params[:id]))
+  end
 end
