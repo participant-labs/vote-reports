8eb99709332b3126d1cfd1549e25342ea5ec0488
James Croft <James.R.Croft@googlemail.com>
2009-11-22 10:46:59 -0500

    flash

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,32 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+// 
+// $.fn.fadeOutSoon = function(when, length) {
+//   if (typeof when == "undefined") when = 5000;
+//   if (typeof length == "undefined") length = 1000;
+//   var $this = $(this);
+//   setTimeout(function() {
+//     $this.fadeOut(length)
+//   }, when);
+//   return $this;
+// };
+;(function($) {
+  $.fn.fadeOutSoon = function(when, length) {
+    if (typeof when == "undefined") when = 5000;
+    if (typeof length == "undefined") length = 1000;
+    var $this = $(this);
+    setTimeout(function() {
+      $this.fadeOut(length)
+    }, when);
+    return $this;
+  };
+  
+  $(document).ready(function(){
+
+    $('.flash').fadeOutSoon(1000);
+
+
+  });
+})(jQuery);
+
+
