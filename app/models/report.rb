3d2f8550dc3eb986d142c84640b485652b17004e
Ben Woosley <ben.woosley@gmail.com>
2009-11-22 11:54:25 -0500

    Add Reports::BillsController and reports/bills/new form

--- app/models/report.rb
+++ app/models/report.rb
@@ -1,6 +1,10 @@
 class Report < ActiveRecord::Base
   belongs_to :user
-  
+  has_many :bill_criteria
+  has_many :bills, :through => :bill_criteria
+
   validates_presence_of :user_id
   validates_presence_of :name
+
+  accepts_nested_attributes_for :bill_criteria
 end
