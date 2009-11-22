f5ecf1fee6916d245820c15112cc174fa39f3642
James Croft <James.R.Croft@googlemail.com>
2009-11-22 14:53:26 -0500

    removed memcache

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -16,4 +16,4 @@ config.action_controller.perform_caching             = false
 # Don't care if the mailer can't send
 config.action_mailer.raise_delivery_errors = false
 
-ActionController::Base.cache_store = :mem_cache_store, "127.0.0.1"
+#ActionController::Base.cache_store = :mem_cache_store, "127.0.0.1"
