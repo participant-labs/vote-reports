7abfa123d8da721d2557dd07e953fd8e71ce7c20
Ben Woosley <ben.woosley@gmail.com>
2009-11-24 17:33:27 -0500

    Send non-user visitors to users/show to users/reports/show.  users/show will be dashboard, users/reports/show will be public profile.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,6 +7,7 @@ class UsersController < ApplicationController
   
   def show
     @user = User.find(params[:id])
+    redirect_to [@user, :reports] unless current_user == @user
   end
   
   def new
