559bb294e3018bdfe3bc153f70daa1449f733ea5
Ben Woosley <ben.woosley@gmail.com>
2009-11-24 19:30:20 -0500

    Add Browse Reports link to sidebar; extract reports/list to serve

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,8 +1,12 @@
 class ReportsController < ApplicationController
-  before_filter :login_required, :except => :show
+  before_filter :login_required, :except => [:show, :index]
 
   def new
     redirect_to new_user_report_path(current_user)
+  end
+
+  def index
+    @recent_reports = Report.recent
   end
 
   def show
