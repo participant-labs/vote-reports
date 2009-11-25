18d32a3ba1fb4ce54727370b5605a341cf2db949
Ben Woosley <ben.woosley@gmail.com>
2009-11-25 16:07:40 -0500

    Allow params in the access denial redirection specs, to allow for the return_to param

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -80,6 +80,7 @@ def it_denies_access(opts = {})
   end
 
   it "redirects to #{redirect_path}" do
-    response.should redirect_to(eval(redirect_path))
+    response.should be_redirect
+    response.redirect_url.should match(%r{^http://test.host#{eval(redirect_path)}(\?|$)})
   end
 end
