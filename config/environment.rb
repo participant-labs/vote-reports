c2b0ea2f5f3832e342f91fafc7847542335f8de4
Ben Woosley <ben.woosley@gmail.com>
2009-11-29 23:13:07 -0500

    Move activesupport json setup to an initializer

--- config/environment.rb
+++ config/environment.rb
@@ -47,5 +47,3 @@ Rails::Initializer.run do |config|
   # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
   # config.i18n.default_locale = :de
 end
-
-ActiveSupport::JSON.backend = 'JSONGem'
