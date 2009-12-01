5163177925460d4009bac465b373a6fbe19c4d46
Ben Woosley <ben.woosley@gmail.com>
2009-12-01 02:06:06 -0500

    Fix sunlight labs api fetching to properly reference local vs. sunlight names

--- app/models/politician/sunlight_labs.rb
+++ app/models/politician/sunlight_labs.rb
@@ -1,9 +1,16 @@
 class Politician
   module SunlightLabs
-    SUNLIGHT_ATTRIBUTES = %w[bioguide_id congress_office congresspedia_url crp_id district email event_id fax fec_id firstname gender govtrack_id in_office lastname middlename name_suffix nickname party phone senate_class state title twitter_id votesmart_id webform website youtube_url].freeze
+    SUNLIGHT_ATTRIBUTES = %w[bioguide_id congress_office congresspedia_url crp_id district email event_id fax fec_id first_name gender gov_track_id in_office last_name middlename name_suffix nickname party phone senate_class state title twitter_id vote_smart_id webform website youtube_url].freeze
+
+    SUNLIGHT_RENAMES = {
+      'first_name' => 'firstname',
+      'last_name' => 'lastname',
+      'gov_track_id' => 'govtrack_id',
+      'vote_smart_id' => 'votesmart_id'
+    }.freeze
 
     def self.included(base)
-      base.after_create :load_sunlight_labs_data
+      base.after_validation_on_create :load_sunlight_labs_data
     end
 
     def load_sunlight_labs_data
@@ -14,7 +21,7 @@ class Politician
         legislator = legislators.first
         Rails.logger.info "SUNLIGHT: Fetched '#{full_name}' as '#{legislator.firstname} #{legislator.lastname}'"
         SUNLIGHT_ATTRIBUTES.each do |attr|
-          self.send("#{attr}=", politician.send(attr))
+          self.send("#{attr}=", legislator.send(SUNLIGHT_RENAMES.fetch(attr, attr)))
         end
       end
     end
