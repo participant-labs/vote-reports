57b9866eb04fb2a776e096e3838fa7e1ec9a4a42
Ben Woosley <ben.woosley@gmail.com>
2009-12-03 03:29:06 -0500

    Drop descriptions from reports for Heroku's sake

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091202023827) do
+ActiveRecord::Schema.define(:version => 20091203082038) do
 
   create_table "bill_criteria", :force => true do |t|
     t.integer  "bill_id"
