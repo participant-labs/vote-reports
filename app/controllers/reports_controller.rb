d16dd0c1f9d67c8dc23b78b170aa832309a2ba65
Ben Woosley <ben.woosley@gmail.com>
2009-12-05 16:02:08 -0500

    Majorly tighten up the reports controller routes, remove default routing

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,5 +1,5 @@
 class ReportsController < ApplicationController
-  before_filter :login_required, :except => [:show, :index]
+  before_filter :login_required, :except => [:index]
 
   def new
     redirect_to new_user_report_path(current_user)
@@ -7,15 +7,5 @@ class ReportsController < ApplicationController
 
   def index
     @recent_reports = Report.recent.published
-  end
-
-  def show
-    report = Report.find(params[:id])
-    redirect_to user_report_path(report.user, report)
-  end
-
-  def edit
-    report = Report.find(params[:id])
-    redirect_to edit_user_report_path(report.user, report)
   end
 end
