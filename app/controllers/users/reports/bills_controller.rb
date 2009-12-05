70ddcb7478e4a9b72e0eaee1777389f6c9ad9668
Ben Woosley <ben.woosley@gmail.com>
2009-12-05 16:43:33 -0500

    Use friendly_id scopes on all user report finds

--- app/controllers/users/reports/bills_controller.rb
+++ app/controllers/users/reports/bills_controller.rb
@@ -1,6 +1,6 @@
 class Users::Reports::BillsController < ApplicationController
   def new
-    @report = current_user.reports.find(params[:report_id])
+    @report = current_user.reports.find(params[:report_id], :scope => current_user)
     if @report.has_better_id?
       redirect_to user_report_bills_path(current_user, @report), :status => 301
     end
