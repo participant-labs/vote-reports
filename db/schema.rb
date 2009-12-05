204fc93ec271fcaf2cbf28963e2e636c399ba051
Ben Woosley <ben.woosley@gmail.com>
2009-12-04 21:10:25 -0500

    Add the friendly_id gem

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20091202023827) do
+ActiveRecord::Schema.define(:version => 20091205020723) do
 
   create_table "bill_criteria", :force => true do |t|
     t.integer  "bill_id"
@@ -66,6 +66,18 @@ ActiveRecord::Schema.define(:version => 20091202023827) do
     t.datetime "updated_at"
     t.text     "description"
   end
+
+  create_table "slugs", :force => true do |t|
+    t.string   "name"
+    t.integer  "sluggable_id"
+    t.integer  "sequence",                     :default => 1, :null => false
+    t.string   "sluggable_type", :limit => 40
+    t.string   "scope",          :limit => 40
+    t.datetime "created_at"
+  end
+
+  add_index "slugs", ["name", "sluggable_type", "scope", "sequence"], :name => "index_slugs_on_n_s_s_and_s", :unique => true
+  add_index "slugs", ["sluggable_id"], :name => "index_slugs_on_sluggable_id"
 
   create_table "users", :force => true do |t|
     t.string   "email"
