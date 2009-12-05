0eb979ad72d8fe742a307c03c2ddec06a94f79da
Ben Woosley <ben.woosley@gmail.com>
2009-12-05 01:42:41 -0500

    A first crack at Exception notification for web service failures

--- app/models/politician/gov_track.rb
+++ app/models/politician/gov_track.rb
@@ -3,9 +3,11 @@ class Politician
     HEADSHOT_TYPE_TO_SIZE = {:small => '50px', :medium => '100px', :large => '200px'}.freeze
 
     def headshot_url(type = nil)
+      if gov_track_id.blank?
+        notify(ArgumentError.new("Unable to fetch headshot, no gov_track_id for: #{inspect}"))
+        return nil
+      end
       ::GovTrack::Politician.new(gov_track_id).headshot_url(HEADSHOT_TYPE_TO_SIZE[type])
-    rescue ArgumentError
-      nil
     end
   end
 end
