8ad4b2a3d2ededa44a7da24209b4c99c8c8a08a7
Ben Woosley <ben.woosley@gmail.com>
2009-12-06 16:53:44 -0500

    Try again on the base Exceptional module

--- config/initializers/exceptional.rb
+++ config/initializers/exceptional.rb
@@ -1,4 +1,4 @@
 def notify(exception)
   Rails.logger.error(exception.inspect)
-  Exceptional::Api.catch(exception) if defined?(Exceptional::Api)
+  Exceptional.catch(exception) if defined?(Exceptional)
 end
