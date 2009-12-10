0c9d247ef8d4c67898265f1fc5670b7a45a77ac4
Ben Woosley <ben.woosley@gmail.com>
2009-12-10 16:44:24 -0500

    Add a migration to populate ex-legislator data from sunlight

---
+++ db/migrate/20091210214123_populate_out_office_sunlight_legislator_data.rb
@@ -0,0 +1,10 @@
+class PopulateOutOfficeSunlightLegislatorData < ActiveRecord::Migration
+  def self.up
+    Politician.all(:conditions => {:gov_track_id => nil}).each do |politician|
+      politician.load_sunlight_labs_data
+    end
+  end
+
+  def self.down
+  end
+end
