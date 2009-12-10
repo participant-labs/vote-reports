827b4465e1e9663cdd3390c0a21ee62ef20efcb2
Ben Woosley <ben.woosley@gmail.com>
2009-12-10 00:54:58 -0500

    Left out religion

--- db/migrate/20091210045028_add_missing_gov_track_attributes_to_politician.rb
+++ db/migrate/20091210045028_add_missing_gov_track_attributes_to_politician.rb
@@ -2,10 +2,12 @@ class AddMissingGovTrackAttributesToPolitician < ActiveRecord::Migration
   def self.up
     add_column :politicians, :metavid_id, :string
     add_column :politicians, :birthday, :date
+    add_column :politicians, :religion, :string
   end
 
   def self.down
     remove_column :politicians, :metavid_id
     remove_column :politicians, :birthday
+    remove_column :politicians, :religion
   end
 end
