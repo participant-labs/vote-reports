5b553ab59d98860127639044f8ed31b3d5f5f97a
Ben Woosley <ben.woosley@gmail.com>
2009-12-13 12:46:44 -0500

    Reset Politician column info so it's available for following data migration

--- db/migrate/20091213161253_add_missing_sunlight_politician_columns.rb
+++ db/migrate/20091213161253_add_missing_sunlight_politician_columns.rb
@@ -3,6 +3,7 @@ class AddMissingSunlightPoliticianColumns < ActiveRecord::Migration
     Politician.find_by_gov_track_id(412283).try(:update_attributes!, :crp_id => nil)
     Politician.find_by_gov_track_id(400169).try(:update_attributes!, :vote_smart_id => nil)
     add_column :politicians, :official_rss, :string
+    Politician.reset_column_information
   end
 
   def self.down
