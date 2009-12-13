87d4858e38507635315caac33800e5e10bbf96bd
Ben Woosley <ben.woosley@gmail.com>
2009-12-13 14:20:54 -0500

    Log the actual exceptional exception

--- config/initializers/exceptional.rb
+++ config/initializers/exceptional.rb
@@ -2,7 +2,7 @@ def notify_exceptional(exception)
   Rails.logger.error(exception.inspect)
   Exceptional.catch(exception) if defined?(Exceptional)
   nil
-rescue
-  Rails.logger.error("Trouble Notifying Exception of #{exception.inspect}")
+rescue => e
+  Rails.logger.error("Trouble (#{e.inspect}) Notifying Exceptional of #{exception.inspect}")
   nil
 end
