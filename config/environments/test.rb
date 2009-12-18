f1b0a0c5c9eb1ad45a68814a11ef8f9853a9d730
Ben Woosley <ben.woosley@gmail.com>
2009-12-18 15:51:03 -0500

    Remove mocha, we aren't doing any stubbing/mocking yet

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -30,6 +30,5 @@ config.action_mailer.delivery_method = :test
 config.gem "rspec", :lib => false, :version => ">=1.2.9"
 config.gem "rspec-rails", :lib => false, :version => ">=1.2.9"
 config.gem 'machinist'
-config.gem 'mocha'
 config.gem 'forgery', :source => 'http://gemcutter.org'
 config.gem 'fakeweb'
