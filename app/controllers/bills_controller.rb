137e46fef53ded6b8cfc93597d93a437927f4058
Ben Woosley <ben.woosley@gmail.com>
2009-12-18 20:49:13 -0500

    Add solr and will_paginate action, which mostly works

--- app/controllers/bills_controller.rb
+++ app/controllers/bills_controller.rb
@@ -2,10 +2,10 @@ class BillsController < ApplicationController
   def index
     if @q = params[:q]
       @title = 'Matching Bills'
-      @bills = Bill.fetch_by_query(@q)
+      @bills = Bill.search { fulltext @q }.results
     else
       @title = 'Recent Bills'
-      @bills = Bill.recent
+      @bills = Bill.recent.paginate :page => params[:page], :per_page => 50
     end
   end
 
--- features/support/fake_web.rb
+++ features/support/fake_web.rb
@@ -1,5 +1,7 @@
 module FakeWebSupport
   require Rails.root.join('spec/support/fake_web')
+  
+  FakeWeb.allow_net_connect = %r{^http://localhost:8983/solr/}
 end
 
 World(FakeWebSupport)
