1cf5f48b53d49dbfa285de983332b92be5faeb7c
Ben Woosley <ben.woosley@gmail.com>
2009-12-19 16:53:43 -0500

    Reorganize Bill model

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -4,19 +4,29 @@ class Bill < ActiveRecord::Base
   named_scope :recent, :limit => 25, :order => 'created_at DESC'
 
   has_friendly_id :opencongress_id
-  has_many :rolls, :as => :subject, :dependent => :destroy
-  has_many :amendments, :dependent => :destroy
-  belongs_to :congress
-  belongs_to :sponsor, :class_name => 'Politician'
-
-  validates_presence_of :bill_type, :congress, :sponsor, :gov_track_id
-  validates_uniqueness_of :gov_track_id
 
   searchable do
     text :title, :summary, :gov_track_id, :opencongress_id
     integer :bill_number
     time :introduced_on
   end
+
+  belongs_to :congress
+  belongs_to :sponsor, :class_name => 'Politician'
+
+  has_many :bill_criteria
+  has_many :amendments, :dependent => :destroy
+  has_many :rolls, :as => :subject, :dependent => :destroy
+  has_many :votes, :through => :rolls
+  def politicians
+    Politician.scoped(:joins => {:votes => :roll}, :conditions => {
+      :'rolls.subject_id' => self, :'rolls.subject_type' => 'Bill'
+    }).extend(Vote::Support)
+  end
+
+  validates_presence_of :bill_type, :congress, :sponsor, :gov_track_id
+  validates_uniqueness_of :gov_track_id
+  validates_uniqueness_of :opencongress_id, :allow_nil => true
 
   def bill_type
     self['bill_type'].gsub('.',' ').gsub('H Res','HR').gsub('H R','HR')
@@ -29,15 +39,4 @@ class Bill < ActiveRecord::Base
   def opencongress_url
     "http://www.opencongress.org/bill/#{opencongress_id}/show"
   end
-
-  has_many :bill_criteria, :dependent => :destroy
-  has_many :rolls, :as => :subject
-  has_many :votes, :through => :rolls
-  def politicians
-    Politician.scoped(:joins => {:votes => :roll}, :conditions => {
-      :'rolls.subject_id' => self, :'rolls.subject_type' => 'Bill'
-    }).extend(Vote::Support)
-  end
-
-  validates_uniqueness_of :opencongress_id, :allow_nil => true
 end
