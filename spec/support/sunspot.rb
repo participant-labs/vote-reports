5dc2f014d03c96161b6e5eeab60df1fc920a3106
Ben Woosley <ben.woosley@gmail.com>
2009-12-22 23:03:27 -0500

    Override sunspot to stub with RR

--- spec/support/sunspot.rb
+++ spec/support/sunspot.rb
@@ -1,1 +1,14 @@
 require 'sunspot/spec/extension'
+
+module Sunspot
+  module Rails
+    module Spec
+      module Extension
+        def mock_sunspot
+          stub(Sunspot).index
+          stub(Sunspot).remove_from_index
+        end
+      end
+    end
+  end
+end
