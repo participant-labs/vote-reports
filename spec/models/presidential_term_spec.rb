5af7d5cb05736ab663840eb1f843e9fb06fe9eb4
Ben Woosley <ben.woosley@gmail.com>
2009-12-22 23:04:10 -0500

    Remove pointless scaffold spec for PresidentialTerm

--- spec/models/presidential_term_spec.rb
+++ spec/models/presidential_term_spec.rb
@@ -1,17 +1,4 @@
 require 'spec_helper'
 
 describe PresidentialTerm do
-  before(:each) do
-    @valid_attributes = {
-      :politician_id => 1,
-      :started_on => Date.today,
-      :ended_on => Date.today,
-      :party => "value for party",
-      :url => "value for url"
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    PresidentialTerm.create!(@valid_attributes)
-  end
 end
