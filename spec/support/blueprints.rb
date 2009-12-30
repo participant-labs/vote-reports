3f655fed44f81ab11b3958483827a3ea6df236ac
Ben Woosley <ben.woosley@gmail.com>
2009-12-30 02:50:52 -0500

    Update builders to match new validations

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -1,4 +1,24 @@
 Fixjour :verify => true do
+  def meeting
+    Forgery(:basic).number(:at_least => 103, :at_most => 111)
+  end
+
+  def bill_type
+    Forgery(:basic).text(:at_least => 2, :at_most => 2, :allow_numeric => false, :allow_upper => false)
+  end
+
+  def bill_number
+    Forgery(:basic).number(:at_most => 9999)
+  end
+
+  def opencongress_id
+    "#{meeting}-#{bill_type}#{bill_number}"
+  end
+
+  def gov_track_id
+    "#{bill_type}#{meeting}-#{bill_number}"
+  end
+
   define_builder(User) do |klass, overrides|
     klass.new(
       :email => Forgery(:internet).email_address,
@@ -13,7 +33,14 @@ Fixjour :verify => true do
   end
 
   define_builder(Amendment) do |klass, overrides|
-    klass.new(:title => Forgery(:basic).text, :bill => new_bill, :gov_track_id => rand(1000000))
+    klass.new(
+      :purpose => Forgery(:basic).text,
+      :description => Forgery(:basic).text,
+      :bill => new_bill,
+      :number => rand(1000),
+      :offered_on => "12/13/2009",
+      :chamber => ['h', 's'].rand,
+      :congress => Congress.find_or_create_by_meeting(109))
   end
 
   define_builder(Congress) do |klass, overrides|
@@ -23,8 +50,8 @@ Fixjour :verify => true do
   define_builder(Bill) do |klass, overrides|
     klass.new(
       :title => Forgery(:basic).text,
-      :opencongress_id => Forgery(:basic).text,
-      :gov_track_id => rand(1000000),
+      :opencongress_id => opencongress_id,
+      :gov_track_id => gov_track_id,
       :bill_type => 'hr',
       :sponsor => new_politician,
       :congress => Congress.find_or_create_by_meeting(111)
@@ -32,7 +59,7 @@ Fixjour :verify => true do
   end
 
   define_builder(BillCriterion) do |klass, overrides|
-    klass.new(:report => new_report, :bill => new_bill)
+    klass.new(:report => new_report, :bill => new_bill, :support => true)
   end
 
   define_builder(Politician) do |klass, overrides|
@@ -55,7 +82,7 @@ Fixjour :verify => true do
     klass.new(
       :congress => Congress.find_or_create_by_meeting(111),
       :subject => new_bill,
-      :opencongress_id => Forgery(:basic).text
+      :gov_track_id => Forgery(:basic).text
     )
   end
 
