aeb58e0299ae28003ceaf91bcdf39832a765020b
Ben Woosley <ben.woosley@gmail.com>
2009-12-30 18:01:21 -0500

    Constrain RepresentativeTerms

--- app/models/representative_term.rb
+++ app/models/representative_term.rb
@@ -1,7 +1,7 @@
 class RepresentativeTerm < ActiveRecord::Base
   belongs_to :politician
 
-  validates_presence_of :politician
+  validates_presence_of :politician, :state, :district
 
   def for
     "representing the #{district.ordinalize} district of #{UsState.name_from_abbrev(state)}"
