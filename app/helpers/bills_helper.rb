8ae315f8781c235daaf7061161789a104fd8a510
Ben Woosley <ben.woosley@gmail.com>
2009-12-31 14:38:46 -0500

    Make Bill#title_with_number a helper and make the introduction year optional

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,2 +1,9 @@
 module BillsHelper
+  def bill_title(bill, options = {})
+    if options[:with_date]
+      "#{bill.bill_type.upcase} #{bill.bill_number} (#{bill.introduced_on.year}): #{bill.title}"
+    else
+      "#{bill.bill_type.upcase} #{bill.bill_number}: #{bill.title}"
+    end
+  end
 end
