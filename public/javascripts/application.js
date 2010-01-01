949d4e84df9f6812b4fdd59ff0654eb3ad6020f6
Ben Woosley <ben.woosley@gmail.com>
2010-01-01 18:48:08 -0500

    Move bill summary which can be quite long and opaque into a dialog box

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,15 +1,6 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
 // 
-// $.fn.fadeOutSoon = function(when, length) {
-//   if (typeof when == "undefined") when = 5000;
-//   if (typeof length == "undefined") length = 1000;
-//   var $this = $(this);
-//   setTimeout(function() {
-//     $this.fadeOut(length)
-//   }, when);
-//   return $this;
-// };
 ;(function($) {
   $.fn.fadeOutSoon = function(when, length) {
     if (typeof when == "undefined") when = 5000;
@@ -25,6 +16,16 @@
 
     $('.flash').fadeOutSoon(1000);
 
+    $('[data-dialog]').each(function() {
+        $($(this).attr('data-dialog')).dialog({
+          autoOpen: false,
+          title: $(this).attr('data-dialog-title'),
+          width: 800});
+    });
+    $('[data-dialog]').click(function(event) {
+        $($(event.target).attr('data-dialog')).dialog('open');
+        return false;
+    });
 
   });
 })(jQuery);
