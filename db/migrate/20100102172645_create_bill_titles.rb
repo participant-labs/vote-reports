8da741d3b6fc010fc71c60b299692f0da972fd2f
Ben Woosley <ben.woosley@gmail.com>
2010-01-02 15:56:44 -0500

    Add BillTitle type for collecting numerous Bill#bill_titles

---
+++ db/migrate/20100102172645_create_bill_titles.rb
@@ -0,0 +1,28 @@
+class CreateBillTitles < ActiveRecord::Migration
+  def self.up
+    transaction do
+      create_table :bill_titles do |t|
+        t.text :title, :null => false
+        t.string :title_type, :null => false
+        t.string :as, :null => false
+        t.integer :bill_id, :null => false
+
+        t.timestamps
+      end
+      constrain :bill_titles do |t|
+        t.bill_id :reference => {:bills => :id, :on_delete => :cascade}
+        t.title :not_blank => true
+        t.title_type :not_blank => true
+        t.as :not_blank => true
+      end
+      remove_column :bills, :title
+    end
+  end
+
+  def self.down
+    transaction do
+      drop_table :bill_titles
+      add_column :bills, :title, :text
+    end
+  end
+end
