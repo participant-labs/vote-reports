b71c5aea112bb8f80c429bcaa1c2075242cf6841
Ben Woosley <ben.woosley@gmail.com>
2010-01-04 03:09:28 -0500

    Turns out Committee#thomas_name isn't always available

--- db/migrate/20100104011612_create_committees.rb
+++ db/migrate/20100104011612_create_committees.rb
@@ -4,7 +4,7 @@ class CreateCommittees < ActiveRecord::Migration
       t.string :chamber
       t.string :code, :null => false
       t.string :name, :null => false
-      t.string :thomas_name, :null => false
+      t.string :thomas_name
       t.string :ancestry
 
       t.timestamps
@@ -12,7 +12,6 @@ class CreateCommittees < ActiveRecord::Migration
     constrain :committees do |t|
       t.code :not_blank => true, :unique => true
       t.name :not_blank => true
-      t.thomas_name :not_blank => true
     end
     add_index :committees, :ancestry
   end
