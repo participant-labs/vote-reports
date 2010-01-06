d67a0c20e9aa7ea4f66d6106356c415bd3a3330b
Ben Woosley <ben.woosley@gmail.com>
2010-01-06 17:48:46 -0500

    A few cosponsorships don't have dates recorded

--- db/migrate/20091228023240_create_cosponsorships.rb
+++ db/migrate/20091228023240_create_cosponsorships.rb
@@ -4,13 +4,14 @@ class CreateCosponsorships < ActiveRecord::Migration
       create_table :cosponsorships do |t|
         t.integer :bill_id, :null => false
         t.integer :politician_id, :null => false
-        t.date :joined_on, :null => false
+        t.date :joined_on
 
         t.timestamps
       end
       constrain :cosponsorships do |t|
         t.bill_id :reference => {:bills => :id, :on_delete => :cascade}
         t.politician_id :reference => {:politicians => :id, :on_delete => :cascade}
+        t[:bill_id, :politician_id].all :unique => true
       end
     end
   end
