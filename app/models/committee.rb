13936a82abe6299e436603cba6d597fcfd54a8c7
Ben Woosley <ben.woosley@gmail.com>
2010-01-06 15:51:44 -0500

    Introduce Committee.find_by_name

--- app/models/committee.rb
+++ app/models/committee.rb
@@ -3,4 +3,8 @@ class Committee < ActiveRecord::Base
 
   has_many :names, :class_name => 'CommitteeName'
   has_many :memberships, :class_name => 'CommitteeMembership'
+
+  named_scope :find_by_name, lambda {|name|
+    {:joins => :names, :conditions => {:'committee_names.name' => name}, :limit => 1}
+  }
 end
