f649518f54174d123ddb5678a489ae842a80e5e5
Ben Woosley <ben.woosley@gmail.com>
2010-01-08 15:52:54 -0500

    Update Amendment constraints, move them up before Amendment import and remove equivalent validations

--- app/models/amendment.rb
+++ app/models/amendment.rb
@@ -5,11 +5,6 @@ class Amendment < ActiveRecord::Base
 
   has_many :rolls, :as => :subject, :dependent => :destroy
 
-  validates_presence_of :bill, :number, :chamber, :offered_on
-  validates_uniqueness_of :number, :scope => [:chamber, :congress_id]
-  validates_uniqueness_of :number, :scope => :bill_id
-  validates_uniqueness_of :sequence, :scope => :bill_id, :allow_nil => true
-
   default_scope :select => 'DISTINCT amendments.*', :joins => :rolls
 
   def title
