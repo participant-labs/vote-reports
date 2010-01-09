dec8258001b07cf1c21b82c76479f0cea339d7b0
Ben Woosley <ben.woosley@gmail.com>
2010-01-09 16:29:01 -0500

    Don't show 'View on OpenCongress' if the bill isn't for the current meeting; introduce Congress.current_meeting and #current? to back this up

--- spec/models/congress_spec.rb
+++ spec/models/congress_spec.rb
@@ -1,6 +1,25 @@
 require File.dirname(__FILE__) + '/../spec_helper'
 
 describe Congress do
+  describe ".current_meeting" do
+    it "should return the proper meeting based on the current date" do
+      {
+        "1/9/2009" => 111,
+        "1/9/2010" => 111,
+        "1/2/1993" => 102,
+        "1/3/1993" => 103,
+        "1/4/1993" => 103,
+        "1/3/1994" => 103,
+        "1/2/1997" => 104,
+        "1/3/1997" => 105
+      }.each do |date, meeting|
+        date = Date.parse(date)
+        stub(Date).today { date }
+        Congress.current_meeting.should == meeting
+      end
+    end
+  end
+
   describe "#senators" do
     it "should return the senators in office during this Congress"
   end
