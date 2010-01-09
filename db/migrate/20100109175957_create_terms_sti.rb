dc2fd7afcd57384b7d9f1634e1af36c6b69607cd
Ben Woosley <ben.woosley@gmail.com>
2010-01-09 14:25:25 -0500

    Offer senate_class as a separate column on the Terms table

--- db/migrate/20100109175957_create_terms_sti.rb
+++ db/migrate/20100109175957_create_terms_sti.rb
@@ -8,7 +8,9 @@ class CreateTermsSti < ActiveRecord::Migration
       add_column :representative_terms, :created_at, :datetime
       add_column :representative_terms, :updated_at, :datetime
       RepresentativeTerm.update_all(:created_at => DateTime.now)
-      
+
+      add_column :representative_terms, :senate_class, :integer
+
       add_column :representative_terms, :type, :string
       constrain :representative_terms do |t|
         t[:type].all :whitelist => %w[RepresentativeTerm SenateTerm PresidentialTerm]
@@ -25,10 +27,10 @@ class CreateTermsSti < ActiveRecord::Migration
       end
 
       RepresentativeTerm.reset_column_information
-      cols = ["party_id", "created_at", "politician_id", "district", "updated_at", "ended_on", "url", "type", "started_on", "state"]
+      cols = ['senate_class', "party_id", "created_at", "politician_id", "district", "updated_at", "ended_on", "url", "type", "started_on", "state"]
       RepresentativeTerm.import_without_validations_or_callbacks cols, (SenateTerm.all.map do |term|
         attrs = term.attributes.merge('type' => 'SenateTerm')
-        cols.map {|col| attrs[col == 'district' ? 'senate_class' : col] }
+        cols.map {|col| attrs[col] }
       end)
       RepresentativeTerm.import_without_validations_or_callbacks cols, (PresidentialTerm.all.map do |term|
         attrs = term.attributes.merge("type" => 'PresidentialTerm')
@@ -41,6 +43,11 @@ class CreateTermsSti < ActiveRecord::Migration
 
   def self.down
     rename_table :terms, :representative_terms
+    RepresentativeTerm.delete_all(:type => 'SenateTerm')
+    RepresentativeTerm.delete_all(:type => 'PresidentialTerm')
+    constrain :representative_terms do |t|
+      t.state :not_blank => true
+    end
     remove_column :representative_terms, :created_at
     remove_column :representative_terms, :updated_at
     remove_column :representative_terms, :type
