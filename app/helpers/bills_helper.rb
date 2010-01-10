6c304a8d2ac2c39e6a21d001fa2708cf84a8b7cb
Ben Woosley <ben.woosley@gmail.com>
2010-01-10 18:41:48 -0500

    Remove Bill#bill_titles in favor fo Bill#titles, replace Bill#title with Bill#titles#best

--- app/helpers/bills_helper.rb
+++ app/helpers/bills_helper.rb
@@ -1,9 +1,9 @@
 module BillsHelper
   def bill_title(bill, options = {})
     if options[:with_date]
-      "#{bill.bill_type.to_s} #{bill.bill_number} (#{bill.introduced_on.year}): #{bill.title}"
+      "#{bill.bill_type.to_s} #{bill.bill_number} (#{bill.introduced_on.year}): #{bill.titles.best}"
     else
-      "#{bill.bill_type.to_s} #{bill.bill_number}: #{bill.title}"
+      "#{bill.bill_type.to_s} #{bill.bill_number}: #{bill.titles.best}"
     end
   end
 end
