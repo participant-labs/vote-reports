aced03a311477629eef13928b3cd1e84f803149b
Ben Woosley <ben.woosley@gmail.com>
2010-01-11 00:01:24 -0500

    Politician builder understands & splits :name; use more general 'politician records' step over 'politicians' step.

--- features/step_definitions/politician_steps.rb
+++ features/step_definitions/politician_steps.rb
@@ -1,9 +1,3 @@
-Given /^the following politicians?:$/ do |table|
-  table.hashes.each do |row|
-    create_politician(row.symbolize_keys)
-  end
-end
-
 Given /^the following representative terms for "(.+)":$/ do |name, table|
   politician = Politician.find_by_full_name(name)
   table.hashes.each do |row|
