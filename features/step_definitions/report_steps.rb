09328e0500d0c24c2d541469afaa3da6dc604784
Ben Woosley <ben.woosley@gmail.com>
2010-01-11 17:11:31 -0500

    Add create_published_report helper

--- features/step_definitions/report_steps.rb
+++ features/step_definitions/report_steps.rb
@@ -11,20 +11,16 @@ Given /^I have a report named "([^\"]*)"$/ do |name|
 end
 
 Given /^a published report named "([^\"]*)"$/ do |name|
-  bill = create_bill
-  report = create_report(:name => name)
-  create_bill_criterion(:bill => bill, :report => report)
+  create_published_report(:name => name)
 end
 
 Given /^I have a published report named "([^\"]*)"$/ do |name|
-  bill = create_bill
-  report = create_report(:name => name, :user => current_user)
-  create_bill_criterion(:bill => bill, :report => report)
+  create_published_report(:name => name, :user => current_user)
 end
 
 Given /^I have the following published reports?:$/ do |table|
   table.hashes.each do |row|
-    create_report(row.symbolize_keys.merge(:user => current_user))
+    create_published_report(row.symbolize_keys.merge(:user => current_user))
   end
 end
 
