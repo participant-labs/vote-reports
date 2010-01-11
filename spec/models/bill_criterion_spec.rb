998c98eb3bddf14382ea79f2142df4c53acf5f24
Ben Woosley <ben.woosley@gmail.com>
2010-01-11 02:10:26 -0500

    Remove specs for old BillCriterion#oppose=

--- spec/models/bill_criterion_spec.rb
+++ spec/models/bill_criterion_spec.rb
@@ -5,18 +5,6 @@ describe BillCriterion do
     @bill_criterion = create_bill_criterion
   end
 
-  describe "#oppose=" do
-    it "should interpret '1' as support == false" do
-      @bill_criterion.oppose = '1'
-      @bill_criterion.support.should == false
-    end
-
-    it "should interpret true as support == false" do
-      @bill_criterion.oppose = true
-      @bill_criterion.support.should == false
-    end
-  end
-
   describe "#support=" do
     it "should interpret '1' as true" do
       @bill_criterion.support = '1'
