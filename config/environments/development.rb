19bb3e9be9686c8078a316d5298e5b1f0eec648e
Ben Woosley <ben.woosley@gmail.com>
2010-01-12 16:26:48 -0500

    Reshuffle gems

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -17,3 +17,5 @@ config.action_controller.perform_caching             = false
 config.action_mailer.raise_delivery_errors = false
 
 #ActionController::Base.cache_store = :mem_cache_store, "127.0.0.1"
+config.gem 'nokogiri'
+config.gem 'fastercsv'
