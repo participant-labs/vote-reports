3cda02e49e05ebce1b490a5318f146b54cbe152d
Ben Woosley <ben.woosley@gmail.com>
2010-01-14 12:34:52 -0500

    Show 30 rather than 60 bill on the politician show page

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -9,8 +9,9 @@ class PoliticiansController < ApplicationController
     if @politician.has_better_id?
       redirect_to politician_path(@politician), :status => 301
     end
-    @supported_bills = @politician.bills.supported.paginate(:page => params[:supported_page])
-    @opposed_bills = @politician.bills.opposed.paginate(:page => params[:opposed_page])
+    per_page = Bill.per_page / 2
+    @supported_bills = @politician.bills.supported.paginate(:page => params[:supported_page], :per_page => per_page)
+    @opposed_bills = @politician.bills.opposed.paginate(:page => params[:opposed_page], :per_page => per_page)
   end
 
 end
