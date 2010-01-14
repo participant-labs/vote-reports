509c5961d381e26070466281cc77c607318d818e
Ben Woosley <ben.woosley@gmail.com>
2010-01-14 18:36:55 -0500

    Remove roll friendly id temporarily so that migration is executable

--- app/models/roll.rb
+++ app/models/roll.rb
@@ -3,19 +3,19 @@ class Roll < ActiveRecord::Base
   belongs_to :subject, :polymorphic => true
   belongs_to :congress
 
-  has_friendly_id :friendly_id
+  # has_friendly_id :friendly_id
 
   named_scope :by_voted_at, :order => "voted_at DESC"
 
-  class << self
-    def find_by_friendly_id(friendly_id, options = {})
-      year, where, number = friendly_id.match(/(\d+)-([hs])(\d+)/).captures
-      where = (where == 'h' ? 'house' : 'senate')
-      find_by_where_and_year_and_number(where, year, number, options)
-    end
-  end
-
-  def friendly_id
-    "#{year}-#{where.first}#{number}" if year.present? && where.present? && number.present?
-  end
+  # class << self
+  #   def find_by_friendly_id(friendly_id, options = {})
+  #     year, where, number = friendly_id.match(/(\d+)-([hs])(\d+)/).captures
+  #     where = (where == 'h' ? 'house' : 'senate')
+  #     find_by_where_and_year_and_number(where, year, number, options)
+  #   end
+  # end
+  # 
+  # def friendly_id
+  #   "#{year}-#{where.first}#{number}" if year.present? && where.present? && number.present?
+  # end
 end
