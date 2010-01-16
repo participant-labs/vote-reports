0b0ea9995bfb7e3205828f393143a6377cf1206d
Ben Woosley <ben.woosley@gmail.com>
2010-01-15 19:55:00 -0500

    Add a bunch of Roll data to roll show

--- app/models/vote.rb
+++ app/models/vote.rb
@@ -6,6 +6,11 @@ class Vote < ActiveRecord::Base
   validates_presence_of :politician, :roll
   validates_inclusion_of :vote, :in => %w[ + - P 0 ]
 
+  named_scope :aye, :conditions => {:vote => '+'}
+  named_scope :nay, :conditions => {:vote => '-'}
+  named_scope :present, :conditions => {:vote => 'P'}
+  named_scope :not_voting, :conditions => {:vote => '0'}
+
   def aye?
     vote == '+'
   end
