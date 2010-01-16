4807784f1063333d89a6f55cb835b5063d61d7fa
Ben Woosley <ben.woosley@gmail.com>
2010-01-16 03:12:03 -0500

    Narrow Roll#roll_types down a bit, where there are obvious equivalents and it gets us closer to easily selecting on passage

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100114223827) do
+ActiveRecord::Schema.define(:version => 20100116015937) do
 
   create_table "amendments", :force => true do |t|
     t.integer  "bill_id",      :null => false
