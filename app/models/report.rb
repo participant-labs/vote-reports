3647bb44cca9dbee91aad45bd0fd68ee9f8f889b
Ben Woosley <ben.woosley@gmail.com>
2010-01-18 15:41:13 -0500

    Fix score dilution wrt the number of applicable bill criteria (but not the number of applicable rolls within a bill criteria)

--- app/models/report.rb
+++ app/models/report.rb
@@ -39,15 +39,14 @@ class Report < ActiveRecord::Base
 
   def generate_scores!
     criteria = bill_criteria.active
-    all_scores = criteria.inject(Hash.new(0.0)) do |scores, bill_criterion|
+    all_scores = criteria.inject(Hash.new([])) do |scores, bill_criterion|
       bill_criterion.score.each_pair do |politician, score|
-        scores[politician] += score
+        scores[politician] += [score]
       end
       scores
     end
-    total = criteria.size
-    all_scores.each_pair do |politician, score|
-      all_scores[politician] = (50.0 * score)/total + 50.0
+    all_scores.each_pair do |politician, scores|
+      all_scores[politician] = 50.0 * (scores.sum / scores.size) + 50.0
     end
     all_scores.to_a.sort_by(&:last).reverse
   end
