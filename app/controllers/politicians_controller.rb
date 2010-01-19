9783c360b7503300bd82c9c409dd3ae5472b0412
Ben Woosley <ben.woosley@gmail.com>
2010-01-19 02:57:15 -0500

    Optimize PoliticansController#show db access.

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -5,10 +5,11 @@ class PoliticiansController < ApplicationController
   end
 
   def show
-    @politician = Politician.find(params[:id], :include => {:politician_terms => :party})
+    @politician = Politician.find(params[:id])
     if @politician.has_better_id?
       redirect_to politician_path(@politician), :status => 301
     end
+    @terms = @politician.terms.by_ended_on.all(:include => :party)
     per_page = Bill.per_page / 2
     @supported_bills = @politician.supported_bills.paginate(:page => params[:supported_page], :per_page => per_page, :include => :titles)
     @opposed_bills = @politician.opposed_bills.paginate(:page => params[:opposed_page], :per_page => per_page, :include => :titles)
