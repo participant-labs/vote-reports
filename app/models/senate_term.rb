85cf87674a4ae9e4b82e251f81c98cba8edcf108
Ben Woosley <ben.woosley@gmail.com>
2010-01-19 14:59:20 -0500

    Move all term states onto the UsState table

--- app/models/senate_term.rb
+++ app/models/senate_term.rb
@@ -1,4 +1,6 @@
 class SenateTerm < PoliticianTerm
   attr_accessible :politician, :senate_class, :state, :started_on, :ended_on, :created_on, :updated_on, :url, :party
+  belongs_to :state, :class_name => 'UsState', :foreign_key => :us_state_id
+
   validates_presence_of :senate_class, :state
 end
