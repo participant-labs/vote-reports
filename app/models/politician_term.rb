f3ff7c7b365760d81abd97569c643ed23009a074
Ben Woosley <ben.woosley@gmail.com>
2010-01-19 17:01:48 -0500

    Use :joins => :state to optimize term-based state lookup

--- app/models/politician_term.rb
+++ app/models/politician_term.rb
@@ -3,14 +3,15 @@ class PoliticianTerm < ActiveRecord::Base
 
   belongs_to :politician
   belongs_to :party
+  belongs_to :state, :class_name => 'UsState', :foreign_key => :us_state_id
   validates_presence_of :politician
 
   named_scope :by_ended_on, :order => "ended_on DESC"
   named_scope :congressional, :conditions => {:type => ['RepresentativeTerm', 'SenateTerm']}
 
   class << self
-    def latest
-      by_ended_on.first
+    def latest(options = {})
+      by_ended_on.first(options)
     end
   end
 
--- app/models/senate_term.rb
+++ app/models/senate_term.rb
@@ -1,6 +1,5 @@
 class SenateTerm < PoliticianTerm
   attr_accessible :politician, :senate_class, :state, :started_on, :ended_on, :created_on, :updated_on, :url, :party
-  belongs_to :state, :class_name => 'UsState', :foreign_key => :us_state_id
 
   validates_presence_of :senate_class, :state
 
