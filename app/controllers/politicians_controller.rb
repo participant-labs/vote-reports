0e830b6fe607932d1e666456f7f7b7d730d666f8
Ben Woosley <ben.woosley@gmail.com>
2010-01-19 23:38:03 -0500

    Render proper bill partial on politician support/opposition bills

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -5,14 +5,14 @@ class PoliticiansController < ApplicationController
   end
 
   def show
-    @politician = Politician.find(params[:id])
+    @politician = Politician.find(params[:id], :include => :state)
     if @politician.has_better_id?
       redirect_to politician_path(@politician), :status => 301
     end
-    @terms = @politician.terms.by_ended_on.all(:include => :party)
+    @terms = @politician.terms.by_ended_on.all(:include => [:party, :state])
     per_page = Bill.per_page / 2
-    @supported_bills = @politician.supported_bills.paginate(:page => params[:supported_page], :per_page => per_page, :include => :titles)
-    @opposed_bills = @politician.opposed_bills.paginate(:page => params[:opposed_page], :per_page => per_page, :include => :titles)
+    @supported_bills = @politician.supported_bills.paginate(:page => params[:supported_page], :per_page => per_page, :include => {:titles => :as})
+    @opposed_bills = @politician.opposed_bills.paginate(:page => params[:opposed_page], :per_page => per_page, :include => {:titles => :as})
   end
 
 end
