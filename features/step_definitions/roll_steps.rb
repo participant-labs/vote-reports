9078ecfa3799b81453f116aae9340b8d18a073cb
Ben Woosley <ben.woosley@gmail.com>
2010-01-21 12:30:47 -0500

    Test fixes

--- features/step_definitions/roll_steps.rb
+++ features/step_definitions/roll_steps.rb
@@ -4,7 +4,7 @@ end
 
 Given /^(bill "[^\"]*") has the following rolls?:$/ do |bill, table|
   table.hashes.each do |hash|
-    if hash['voted_at']
+    if hash['voted_at'].to_s.include?('.')
       hash['voted_at'] = eval(hash['voted_at'])
     end
     create_roll(hash.merge(:subject => bill))
--- spec/models/report_score_evidence_spec.rb
+++ spec/models/report_score_evidence_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
 describe ReportScoreEvidence do
-  before(:each) do
-    @valid_attributes = {
-      :report_score_id => 1,
-      :roll_id => 1,
-      :bill_criterion_id => 1
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    ReportScoreEvidence.create!(@valid_attributes)
-  end
 end
