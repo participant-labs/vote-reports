2ce9dfbc1a3f92fc4856e09d328a57653c9f1847
Ben Woosley <ben.woosley@gmail.com>
2010-01-22 18:23:31 -0500

    Show top and bottom scores separately

--- app/models/report_score.rb
+++ app/models/report_score.rb
@@ -2,4 +2,10 @@ class ReportScore < ActiveRecord::Base
   belongs_to :report
   belongs_to :politician
   has_many :evidence, :class_name => 'ReportScoreEvidence', :dependent => :destroy
+
+  class << self
+    def per_page
+      20
+    end
+  end
 end
