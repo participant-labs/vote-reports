cf75f826d356af6728e42c39e054e5f1a3bdcd4b
Ben Woosley <ben.woosley@gmail.com>
2010-01-22 16:58:11 -0500

    Front-page include optimization

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,7 @@
 class SiteController < ApplicationController
 
   def index
-    @recent_reports = Report.scored.by_updated_at.paginate(:page => params[:page])
+    @recent_reports = Report.scored.by_updated_at.paginate(:page => params[:page], :include => :user)
   end
 
   def about
