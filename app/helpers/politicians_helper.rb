d5166d47c643798e2720073e144e45fb3a42e66a
Ben Woosley <ben.woosley@gmail.com>
2010-01-23 17:16:20 -0500

    Add title to politician show, extract politician_title helper to serve

--- app/helpers/politicians_helper.rb
+++ app/helpers/politicians_helper.rb
@@ -1,2 +1,6 @@
 module PoliticiansHelper
+  def politician_title(politician)
+    state = " (#{politician.state.abbreviation})" if politician.state
+    "#{politician.short_title} #{politician.full_name}#{state}"
+  end
 end
