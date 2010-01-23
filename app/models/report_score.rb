079c0ff7e4b8a2d5bd9fb874cdbb61a3fb3a0963
Ben Woosley <ben.woosley@gmail.com>
2010-01-22 19:24:23 -0500

    On reports/show, bail earlier if the id is old, and introduce ReportScore.bottom named score for lowest scores first

--- app/models/report_score.rb
+++ app/models/report_score.rb
@@ -3,6 +3,8 @@ class ReportScore < ActiveRecord::Base
   belongs_to :politician
   has_many :evidence, :class_name => 'ReportScoreEvidence', :dependent => :destroy
 
+  named_scope :bottom, :order => :score
+
   named_scope :with_evidence, :include => {
     :politician => :state,
     :evidence => [{:vote => {:roll => {:subject => {:titles => :as}}}}, :bill_criterion]
