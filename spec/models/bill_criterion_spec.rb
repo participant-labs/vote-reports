795bcc566fae52ab79a4362fa86da504f42b7cb8
Ben Woosley <ben.woosley@gmail.com>
2010-01-24 04:29:04 -0500

    Don't rescore report on BillCriterion update if support hasn't changed

--- spec/models/bill_criterion_spec.rb
+++ spec/models/bill_criterion_spec.rb
@@ -25,4 +25,32 @@ describe BillCriterion do
       (5.years + 3.months + (36.5).days).ago.to_date.years_until(Date.today).should be_close(5.35, 0.003)
     end
   end
+
+  describe "#rescore_report!" do
+    before do
+      @report = create_report
+    end
+
+    context "on create" do
+      it "should be called" do
+        criterion = new_bill_criterion(:report => @report)
+        mock(@report).rescore!
+        criterion.save!
+      end
+    end
+
+    context "on update" do
+      it "should be called if support is changed" do
+        criterion = create_bill_criterion(:report => @report, :support => true)
+        mock(@report).rescore!
+        criterion.update_attributes(:support => false)
+      end
+
+      it "should not be called if support is not changed" do
+        criterion = create_bill_criterion(:report => @report)
+        dont_allow(@report).rescore!
+        criterion.update_attributes(:explanatory_url => 'http://google.com')
+      end
+    end
+  end
 end
