5206894446f487be138a72b1babf40cf0debcb94
Ben Woosley <ben.woosley@gmail.com>
2010-01-24 17:11:02 -0500

    Initial attempt at bundler support

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,13 +6,21 @@ set :repository, 'git@github.com:Empact/vote-reports.git'
 
 namespace :vlad do
   desc "custom deploy"
-  task :deploy => [:update, :symlinks, :migrate, :touch_restart]
+  task :deploy => [:update, :symlinks, :gem_bundle, :migrate, :touch_restart]
 
   remote_task :touch_restart, :roles => :app do
     run "touch #{current_release}/tmp/restart.txt"
   end
 
   remote_task :symlinks, :roles => :app do
-    run "ln -s #{shared_path}/config/database.yml #{current_release}/config/database.yml"
+    run [
+      "ln -s #{shared_path}/config/database.yml #{current_release}/config/database.yml",
+      "ln -s #{shared_path}/vendor/bundler_gems #{current_release}/vendor/bundler_gems",
+      "ln -s #{shared_path}/bin #{current_release}/bin"
+    ].join(' && ')
+  end
+
+  remote_task :gem_bundle, :roles => :app do
+    run "gem bundle --manifest #{current_release}/Gemfile"
   end
 end
