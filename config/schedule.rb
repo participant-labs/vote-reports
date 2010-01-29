03632571d8021260459a3f5574c5340cd25da5c5
Ben Woosley <ben.woosley@gmail.com>
2010-01-28 19:42:12 -0500

    Add cron task for full backup once a week

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,9 +12,9 @@
 #   runner "MyModel.some_method"
 #   rake "some:great:rake:task"
 # end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+
+every 7.days do
+  rake "db:backup:full"
+end
 
 # Learn more: http://github.com/javan/whenever
