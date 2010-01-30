6d3777aeb5042e5f3e5512b5b95ba7d46f0c576c
Ben Woosley <ben.woosley@gmail.com>
2010-01-29 22:51:41 -0500

    Show more appropriate messaging on report bill add if this is the first use, on bill creation

--- app/controllers/users/reports/bills_controller.rb
+++ app/controllers/users/reports/bills_controller.rb
@@ -2,6 +2,7 @@ class Users::Reports::BillsController < ApplicationController
   before_filter :is_report_owner
 
   def new
+    @new_report = true if params[:new_report]
     @report = current_user.reports.find(params[:report_id], :scope => current_user)
     if @report.has_better_id?
       redirect_to new_user_report_bills_path(current_user, @report), :status => 301
