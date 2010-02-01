d1df17a1306160825b53cbf22525d6ac67e71921
Ben Woosley <ben.woosley@gmail.com>
2010-01-31 23:02:44 -0500

    Add UsState#*_terms, #senators and #representatives associations, and DistrictZipCode#district and #state association

--- app/models/us_state.rb
+++ app/models/us_state.rb
@@ -2,4 +2,13 @@ class UsState < ActiveRecord::Base
   def unincorporated?
     state_type != 'state'
   end
+
+  has_many :politician_terms
+  alias_attribute :terms, :politician_terms
+
+  has_many :representative_terms
+  has_many :representatives, :through => :representative_terms, :source => :politician
+
+  has_many :senate_terms
+  has_many :senators, :through => :senate_terms, :source => :politician
 end
