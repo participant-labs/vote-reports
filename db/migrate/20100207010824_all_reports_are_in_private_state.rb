f0899b05d9fd19648e385c0b557358e05b45af69
Ben Woosley <ben.woosley@gmail.com>
2010-02-07 00:23:52 -0500

    Change migration to publish reports which meet the criteria, so we don't launch previously public things into obscurity

--- db/migrate/20100207010824_all_reports_are_in_private_state.rb
+++ db/migrate/20100207010824_all_reports_are_in_private_state.rb
@@ -1,6 +1,9 @@
 class AllReportsAreInPrivateState < ActiveRecord::Migration
   def self.up
     Report.update_all(:state => 'private')
+    Report.all.each do |report|
+      report.publish
+    end
   end
 
   def self.down
