34ee7c17cf773f18f401d73756d50c1956a045ec
Ben Woosley <ben.woosley@gmail.com>
2010-02-12 14:42:07 -0500

    Link copyright to participant labs

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,10 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
 
+  def participant_labs_path
+    'http://participantlabs.com/'
+  end
+
   def blog_path
     'http://blog.votereports.org/'
   end
