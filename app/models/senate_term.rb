1c024269f1bccee538de63e1d276e19ca15315c3
Ben Woosley <ben.woosley@gmail.com>
2010-02-14 10:48:30 -0500

    Do away with PoliticianTerm STI base class and rename the equivalent module to match

--- app/models/senate_term.rb
+++ app/models/senate_term.rb
@@ -1,5 +1,5 @@
 class SenateTerm < ActiveRecord::Base
-  include PoliticianTermStuff
+  include PoliticianTerm
   belongs_to :state, :class_name => 'UsState', :foreign_key => :us_state_id
 
   validates_presence_of :senate_class, :state
