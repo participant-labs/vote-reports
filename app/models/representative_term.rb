dc9596eced8672d6113b4b9fc3552181b3b0c764
Ben Woosley <ben.woosley@gmail.com>
2010-02-14 12:16:39 -0500

    Fix RepresentativeTerm#place and fixjour creation; also sort_by! doesn't exist

--- app/models/representative_term.rb
+++ app/models/representative_term.rb
@@ -4,15 +4,13 @@ class RepresentativeTerm < ActiveRecord::Base
   belongs_to :district
   delegate :state, :to => :district
 
-  validates_presence_of :district
+  validates_presence_of :district, :state
 
   def title
     "#{'Delegate ' if state.unincorporated? }Representative"
   end
 
   def place
-    district = self.district == 0 ? 'at-large' : self.district.ordinalize if self.district
-    district = "the #{district} district of " if district
-    "for #{district}#{state.full_name}"
+    "for #{district.full_name}"
   end
 end
