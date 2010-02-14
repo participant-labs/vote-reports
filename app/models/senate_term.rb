d091404156e0d6edf84dcbd93266e270f8d11c24
Ben Woosley <ben.woosley@gmail.com>
2010-02-14 02:57:29 -0500

    Move senate terms to their own table

--- app/models/senate_term.rb
+++ app/models/senate_term.rb
@@ -1,5 +1,6 @@
-class SenateTerm < PoliticianTerm
-  attr_accessible :politician, :senate_class, :state, :started_on, :ended_on, :created_on, :updated_on, :url, :party
+class SenateTerm < ActiveRecord::Base
+  include PoliticianTermStuff
+  belongs_to :state, :class_name => 'UsState', :foreign_key => :us_state_id
 
   validates_presence_of :senate_class, :state
 
