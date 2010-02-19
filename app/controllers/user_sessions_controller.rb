25c2e7ff043b5f8e4f0a5201decbde1a35655a87
Ben Woosley <ben.woosley@gmail.com>
2010-02-19 14:28:50 -0500

    Setup rpx login support as described in readme

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -7,8 +7,16 @@ class UserSessionsController < ApplicationController
   def create
     @user_session = UserSession.new(params[:user_session])
     if @user_session.save
-      flash[:notice] = "Logged in successfully"
-      redirect_to (params[:return_to].present? ? params[:return_to] : current_user)
+      if @user_session.new_registration?
+        flash[:notice] = "Welcome! As a new user, please review your registration details before continuing.."
+        redirect_to edit_user_path(current_user)
+      elsif @user_session.registration_complete?
+        flash[:notice] = "Logged in successfully"
+        redirect_to (params[:return_to].present? ? params[:return_to] : current_user)
+      else
+        flash[:notice] = "Welcome back! Please complete required registration details before continuing.."
+        redirect_to edit_user_path(current_user)
+      end
     else
       render :action => 'new'
     end
--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -1,4 +1,21 @@
 class UserSession < Authlogic::Session::Base
   find_by_login_method :find_by_username_or_email
   generalize_credentials_error_messages true
+  rpx_key RPX_API_KEY
+
+private
+
+  # map_rpx_data maps additional fields from the RPX response into the user object
+  # override this in your session controller to change the field mapping
+  # see https://rpxnow.com/docs#profile_data for the definition of available attributes
+  #
+  def map_rpx_data
+    # map core profile data using authlogic indirect column names
+    if attempted_record.send(klass.login_field).blank?
+      attempted_record.send(:"#{klass.login_field}=", @rpx_data['profile']['preferredUsername'] )
+    end
+    if attempted_record.send(klass.email_field).blank?
+      attempted_record.send(:"#{klass.email_field}=", @rpx_data['profile']['email'] ) 
+    end
+  end
 end
