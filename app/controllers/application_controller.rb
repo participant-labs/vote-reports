80f52e3eda4c61e4255b7e247f523337806dc3ea
Ben Woosley <ben.woosley@gmail.com>
2010-02-20 14:11:46 -0500

    Add some helpers in the style of the authlogic example; username and email auth should be handled by authlogic

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,10 @@ class ApplicationController < ActionController::Base
   protect_from_forgery # See ActionController::RequestForgeryProtection for details
   geocode_ip_address
 
-  # Scrub sensitive parameters from your log
-  filter_parameter_logging :password
+  helper_method :current_user_session, :current_user
+  filter_parameter_logging :password, :password_confirmation
 
-  helper_method :current_user
+  private
 
   def login_required
     unless current_user
@@ -51,8 +51,6 @@ class ApplicationController < ActionController::Base
     end
   end
 
-  private
-
   def current_user_session
     return @current_user_session if defined?(@current_user_session)
     @current_user_session = UserSession.find
@@ -61,5 +59,14 @@ class ApplicationController < ActionController::Base
   def current_user
     return @current_user if defined?(@current_user)
     @current_user = current_user_session && current_user_session.record
+  end
+
+  def store_location
+    session[:return_to] = request.request_uri
+  end
+
+  def redirect_back_or_default(default)
+    redirect_to(session[:return_to] || default)
+    session[:return_to] = nil
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,6 @@ class User < ActiveRecord::Base
 
   has_many :reports
 
-  validates_presence_of :username, :email
-  validates_uniqueness_of :username, :email
-
   def admin?
     false
   end
