72da44782a26dada2d3b1de3154e44867897cc68
Ben Woosley <ben.woosley@gmail.com>
2010-02-20 23:56:10 -0500

    Access control on users/rpx_identities controller, and some coverage

---
+++ spec/controllers/users/rpx_identities_controller_spec.rb
@@ -0,0 +1,77 @@
+require File.dirname(__FILE__) + '/../../spec_helper'
+
+describe Users::RpxIdentitiesController do
+  describe "POST #create" do
+    describe "on adding a registration to an existing user" do
+      def create_post(user)
+        post :create, :user_id => user.to_param,
+          "token"=>"2a6e5bb00cd01b94752c26e55bbde78242e1514b",
+          "authenticity_token"=>"WJhfd6WXN6DG9ujdhK9YG8pFK5GSPZJfRE43EedI+EQ=",
+          "add_rpx"=>"true",
+          "profile" => {
+            "verifiedEmail" => "ben.woosley@gmail.com",
+            "name" => {
+              "givenName" => "Ben",
+              "familyName" => "Woosley",
+              "formatted" => "Ben Woosley"
+            },
+            "displayName" => "ben.woosley",
+            "preferredUsername" => "ben.woosley",
+            "providerName" => "Google",
+            "identifier" => @identifier,
+            "email" => "ben.woosley@gmail.com"
+          },
+          "stat" => "ok"
+      end
+
+      context "when someone else is attempting to add" do
+        before do
+          @current = create_user
+          login(@current)
+          create_post(create_user)
+        end
+
+        it "should redirect" do
+          response.should redirect_to(user_path(@current))
+        end
+
+        it "should not create an rpx identity" do
+          RPXIdentifier.find_by_identifier(@identifier).should == nil
+          User.find_by_rpx_identifier(@identifier).should == nil
+        end
+      end
+
+      context "when I'm adding to myself" do
+        before do
+          @current = create_user
+          login(@current)
+          @identifier = "https://www.google.com/accounts/o8/id?id=AItOawk9vs1fKTQ-PEoono_hovx95Nt5qWG6LUk"
+        end
+
+        it "should redirect to user page" do
+          create_post(@current)
+          response.should redirect_to(user_path(@current))
+        end
+
+        it "should notify of success" do
+          create_post(@current)
+          flash[:notice].should == "Successfully added login to this account."
+        end
+
+        it "should create an RPXIdentifier" do
+          pending "Tokens are time-sensitive and I don't know how to generate them properly"
+          lambda {
+            create_post(@current)
+          }.should change(RPXIdentifier, :count).by(1)
+        end
+
+        it "should create an rpx_identifier record" do
+          pending "Tokens are time-sensitive and I don't know how to generate them properly"
+          create_post(@current)
+          RPXIdentifier.find_by_identifier(@identifier).user.should == @current
+          User.find_by_rpx_identifier(@identifier).should == @current
+        end
+      end
+    end
+  end
+end
