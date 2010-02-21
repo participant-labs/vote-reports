61ec8b1ceb6229dea9ffdf44e73040df9829aad7
Ben Woosley <ben.woosley@gmail.com>
2010-02-21 03:11:15 -0500

    Sourcing alone didn't work, try adding a config file and requiring it in the initializer

--- config/initializers/rpx.rb
+++ config/initializers/rpx.rb
@@ -1,2 +1,7 @@
-RPX_API_KEY = ENV['RPX_API_KEY']
+begin
+  require Rails.root.join('config/secure_variables')
+rescue LoadError
+end
+
+RPX_API_KEY = ENV['RPX_API_KEY'] unless defined?(RPX_API_KEY)
 RPX_APP_NAME = 'VoteReports'
