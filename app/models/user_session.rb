1588398d5ee6578543fbae6a29677b46e5333514
Ben Woosley <ben.woosley@gmail.com>
2010-02-23 02:27:35 -0500

    Rely on authlogic_rpx defaults where possible, add new_rpx_user for auto-registration

--- app/models/user_session.rb
+++ app/models/user_session.rb
@@ -2,25 +2,10 @@ class UserSession < Authlogic::Session::Base
   find_by_login_method :find_by_username_or_email
   generalize_credentials_error_messages true
   rpx_key RPX_API_KEY
-  validate :user_is_valid
 
 private
 
-  # map_rpx_data maps additional fields from the RPX response into the user object
-  # override this in your session controller to change the field mapping
-  # see https://rpxnow.com/docs#profile_data for the definition of available attributes
-  #
-  def map_rpx_data
-    # map core profile data using authlogic indirect column names
-    if attempted_record.send(klass.login_field).blank?
-      attempted_record.send(:"#{klass.login_field}=", @rpx_data['profile']['preferredUsername'] )
-    end
-    if attempted_record.send(klass.email_field).blank?
-      attempted_record.send(:"#{klass.email_field}=", @rpx_data['profile']['email'] ) 
-    end
-  end
-
-  def user_is_valid
-    errors.add(:user, "is invalid") if user && !user.valid?
+  def new_rpx_user(params)
+    User.new(params[:user])
   end
 end
