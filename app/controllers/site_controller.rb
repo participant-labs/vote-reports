485aab773027395dc9804649b3a7fd2858b35f07
Ben Woosley <ben.woosley@gmail.com>
2010-02-27 01:37:16 -0500

    Move subjects cloud to a partial

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,7 +2,7 @@ class SiteController < ApplicationController
 
   def index
     @recent_reports = Report.published.by_updated_at.paginate(:page => params[:page], :include => :user)
-    @subjects = Subject.for_report(Report.published).for_tag_cloud.all(:limit => 10)
+    @subjects = Subject.for_report(Report.published).for_tag_cloud.all(:limit => 50)
   end
 
   def about
