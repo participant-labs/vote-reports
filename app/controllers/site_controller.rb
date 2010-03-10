3a4ee0100ef73fbfd376021a19f4b896d4b3b1d5
Ben Woosley <ben.woosley@gmail.com>
2010-03-10 17:22:26 -0500

    Rework instant grat lookup such that reports aren't shown when nothing's been selected, and such that we avoid a separate published reports lookup on the base case.

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -2,18 +2,30 @@ class SiteController < ApplicationController
 
   def index
     @recent_reports = Report.published.by_updated_at.paginate(:page => params[:page], :include => :user)
-    @topical_reports = Report.published
-    if params[:tag].present?
-      @topical_reports = @topical_reports.with_subject(params[:tag])
-    end
     if params[:from_where].present?
       @politicians = sought_politicians
-      @topical_reports = @topical_reports.with_scores_for(@politicians)
+      @topical_reports = topical_reports.with_scores_for(@politicians)
+    elsif params[:tag].present?
+      @topical_reports = topical_reports
     end
-    @subjects = Subject.for_report(@topical_reports).for_tag_cloud.all(:limit => 20)
+    @subjects =
+      if @topical_reports
+        Subject.for_report(@topical_reports)
+      else
+        Subject.on_published_reports
+      end.for_tag_cloud.all(:limit => 20)
   end
 
   def about
   end
 
+private
+
+  def topical_reports
+    if params[:tag].present?
+      Report.published.with_subject(params[:tag])
+    elsif params[:from_where].present?
+      Report.published
+    end
+  end
 end
