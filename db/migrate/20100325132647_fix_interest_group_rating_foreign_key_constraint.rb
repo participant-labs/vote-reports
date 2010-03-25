0a71178b550046f32f94ef5b5fd56ef62f05bbdb
Ben Woosley <ben.woosley@gmail.com>
2010-03-25 09:31:48 -0400

    Fix that interest_group_ratings#interest_group_report_id was improperly constrained to interest_groups

---
+++ db/migrate/20100325132647_fix_interest_group_rating_foreign_key_constraint.rb
@@ -0,0 +1,11 @@
+class FixInterestGroupRatingForeignKeyConstraint < ActiveRecord::Migration
+  def self.up
+    deconstrain :interest_group_ratings, :interest_group_report_id, :reference
+    constrain :interest_group_ratings, :interest_group_report_id, :reference => {:interest_group_reports => :id}
+  end
+
+  def self.down
+    deconstrain :interest_group_ratings, :interest_group_report_id, :reference
+    constrain :interest_group_ratings, :interest_group_report_id, :reference => {:interest_groups => :id}
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100325033020) do
+ActiveRecord::Schema.define(:version => 20100325132647) do
 
   create_table "amendments", :force => true do |t|
     t.integer  "bill_id",      :null => false
@@ -298,10 +298,32 @@ ActiveRecord::Schema.define(:version => 20100325033020) do
   add_index "presidential_terms", ["party_id"], :name => "index_presidential_terms_on_party_id"
   add_index "presidential_terms", ["politician_id"], :name => "index_presidential_terms_on_politician_id"
 
+  create_table "report_criteria", :force => true do |t|
+    t.integer  "report_id",          :null => false
+    t.integer  "criteria_report_id", :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "report_criteria", ["criteria_report_id"], :name => "index_report_criteria_on_criteria_report_id"
+  add_index "report_criteria", ["report_id", "criteria_report_id"], :name => "index_report_criteria_on_report_id_and_criteria_report_id", :unique => true
+  add_index "report_criteria", ["report_id"], :name => "index_report_criteria_on_report_id"
+
   create_table "report_delayed_jobs", :force => true do |t|
     t.integer "report_id",      :null => false
     t.integer "delayed_job_id", :null => false
   end
+
+  create_table "report_follows", :force => true do |t|
+    t.integer  "user_id",    :null => false
+    t.integer  "report_id",  :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "report_follows", ["report_id", "user_id"], :name => "index_report_follows_on_report_id_and_user_id", :unique => true
+  add_index "report_follows", ["report_id"], :name => "index_report_follows_on_report_id"
+  add_index "report_follows", ["user_id"], :name => "index_report_follows_on_user_id"
 
   create_table "report_score_evidences", :force => true do |t|
     t.integer  "report_score_id",   :null => false
