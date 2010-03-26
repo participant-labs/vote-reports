5877647034b1862c312358fa87c11ee313439e2c
Ben Woosley <ben.woosley@gmail.com>
2010-03-26 19:15:30 -0400

    Drop bad scaffolded specs

--- spec/models/interest_group_report_spec.rb
+++ spec/models/interest_group_report_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
 describe InterestGroupReport do
-  before(:each) do
-    @valid_attributes = {
-      :interest_group_id => 1,
-      :timespan => "value for timespan",
-      :vote_smart_id => 1
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    InterestGroupReport.create!(@valid_attributes)
-  end
 end
--- spec/models/interest_group_spec.rb
+++ spec/models/interest_group_spec.rb
@@ -1,13 +1,4 @@
 require 'spec_helper'
 
 describe InterestGroup do
-  before(:each) do
-    @valid_attributes = {
-      
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    InterestGroup.create!(@valid_attributes)
-  end
 end
