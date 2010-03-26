c9b1b271a325196085aa71590d464283b5b18deb
Ben Woosley <ben.woosley@gmail.com>
2010-03-26 03:45:33 -0400

    What's up with sunspot?

--- db/migrate/20100325132647_fix_interest_group_rating_foreign_key_constraint.rb
+++ db/migrate/20100325132647_fix_interest_group_rating_foreign_key_constraint.rb
@@ -1,6 +1,6 @@
 class FixInterestGroupRatingForeignKeyConstraint < ActiveRecord::Migration
   def self.up
-    deconstrain :interest_group_ratings, :interest_group_report_id, :reference
+    deconstrain :interest_group_ratings, :interest_group_id, :reference
     constrain :interest_group_ratings, :interest_group_report_id, :reference => {:interest_group_reports => :id}
   end
 
