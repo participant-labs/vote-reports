f054adf83b2b7486b26630b24829e81073f00d40
Ben Woosley <ben.woosley@gmail.com>
2010-03-28 18:54:12 -0400

    Pass around the politician_id rather than the full politician on InterestGroup#report scoring

--- app/models/report/scorer.rb
+++ app/models/report/scorer.rb
@@ -1,14 +1,13 @@
 class Report
   class Scorer < Struct.new(:report_id)
     def perform
-      report = Report.find_by_id(report_id) \
-        || raise("Tried to score missing report #{report_id}")
-      report.criteria_scores.group_by(&:politician).each_pair do |politician, criteria_scores|
+      report = Report.find(report_id)
+      report.criteria_scores.group_by(&:politician_id).each_pair do |politician_id, criteria_scores|
         baseline = criteria_scores.map(&:average_base)
         baseline = baseline.sum / baseline.size
 
         scores = criteria_scores.map {|s| s.score * s.average_base / baseline }
-        score = report.scores.build(:politician => politician, :score => scores.sum / scores.size)
+        score = report.scores.build(:politician_id => politician_id, :score => scores.sum / scores.size)
         criteria_scores.each do |criterion_score|
           criterion_score.build_evidence_on(score)
         end
