9eec70b79cc6c92a043f38f3532d066a885584fb
Ben Woosley <ben.woosley@gmail.com>
2010-04-01 02:19:25 -0400

    Drop the old score objects we're not using anymore

--- app/models/bill_criterion_score.rb
+++
@@ -1,9 +0,0 @@
-class BillCriterionScore
-  attr_reader :politician, :criterion, :events
-
-  def initialize(args)
-    @politician = args.fetch(:politician)
-    @criterion = args.fetch(:bill_criterion)
-    @events = args.fetch(:votes)
-  end
-end
--- app/models/interest_group/score.rb
+++
@@ -1,9 +0,0 @@
-class InterestGroup::Score
-  attr_reader :politician_id, :criterion, :events
-
-  def initialize(args)
-    @politician_id = args.fetch(:politician_id)
-    @criterion = args.fetch(:interest_group)
-    @events = args.fetch(:ratings)
-  end
-end
