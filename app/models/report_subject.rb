a61224b0ceeb0f1468676bf3575ddaba1b82d093
Ben Woosley <ben.woosley@gmail.com>
2010-04-02 14:58:39 -0400

    Cache report subject count data in ReportSubject table

--- app/models/report_subject.rb
+++ app/models/report_subject.rb
@@ -5,4 +5,41 @@ class ReportSubject < ActiveRecord::Base
   # constraints on db:
   # report, subject presence
   # [report, subject] uniqueness
+
+
+  class << self
+    def generate!
+      require 'ar-extensions'
+      require 'ar-extensions/import/postgresql'
+
+      transaction do
+        delete_all
+        subjects = []
+        Report.paginated_each do |report|
+          bill_criteria_subjects = report.bill_criteria_subjects.scoped(
+            :select => "DISTINCT(subjects.id), COUNT(subjects.id) AS count",
+            :group => 'subjects.id').inject({}) do |hash, subject|
+            hash[subject] = subject.count
+            hash
+          end
+          
+          if report.interest_group
+            count = report.interest_group.reports.count
+            report.interest_group.subjects.each do |subject|
+              bill_criteria_subjects[subject] ||= 0
+              bill_criteria_subjects[subject] += count
+            end
+          end
+
+          subjects += bill_criteria_subjects.map do |(subject, count)|
+            [report.id, subject.id, count]
+          end
+        end
+        ReportSubject.import_without_validations_or_callbacks(
+          [:report_id, :subject_id, :count],
+          subjects
+        )
+      end
+    end
+  end
 end
