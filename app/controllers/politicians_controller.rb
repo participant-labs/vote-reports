5477f0622a7ecafe2730fccbd06a7362a7bdb3af
Ben Woosley <ben.woosley@gmail.com>
2010-04-03 18:15:35 -0400

    Paginate Reports shown on politians show

--- app/controllers/politicians_controller.rb
+++ app/controllers/politicians_controller.rb
@@ -20,11 +20,22 @@ class PoliticiansController < ApplicationController
       redirect_to politician_path(@politician), :status => 301
       return
     end
-    @terms = (
-      @politician.representative_terms.all(:include => [:party, :district])  +
-      @politician.senate_terms.all(:include => [:party, :state]) +
-      @politician.presidential_terms.all(:include => :party)
-    ).sort_by(&:ended_on).reverse
+    @scores = @politician.report_scores.published.paginate(:page => params[:page])
+
+    respond_to do |format|
+      format.html {
+        @terms = (
+          @politician.representative_terms.all(:include => [:party, :district])  +
+          @politician.senate_terms.all(:include => [:party, :state]) +
+          @politician.presidential_terms.all(:include => :party)
+        ).sort_by(&:ended_on).reverse
+      }
+      format.js {
+        render :partial => 'politicians/scores/table', :locals => {
+          :scores => @scores
+        }
+      }
+    end
   end
 
 end
