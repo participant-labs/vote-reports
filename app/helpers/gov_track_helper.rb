a6e9b85328fb1d78a21a44512e866a256f9f9e2f
Ben Woosley <ben.woosley@gmail.com>
2010-04-07 00:40:38 -0400

    Add a state map to the state show page, care of govtrack

---
+++ app/helpers/gov_track_helper.rb
@@ -0,0 +1,12 @@
+module GovTrackHelper
+  def gov_track_map_url(object)
+    "http://www.govtrack.us/embed/mapframe.xpd?" +
+      if object.is_a?(UsState)
+        {:state => object.abbreviation}
+      elsif object.is_a?(District)
+        {:state => object.state.abbreviation, :district => object.district}
+      else
+        raise "Not gov-trackable: #{object.inspect}"
+      end.to_param
+  end
+end
