4d06eda6ae62d5fb622499a3b7f211766aeec79b
Ben Woosley <ben.woosley@gmail.com>
2010-04-07 21:35:12 -0400

    Attempt to fix regression such that empty representing means none for the guide, but all elsewhere (e.g., reports)

--- app/helpers/locations_helper.rb
+++ app/helpers/locations_helper.rb
@@ -3,13 +3,13 @@ module LocationsHelper
     (location =~ /^\s*\d{5}([-\s]*\d{4})?\s*$/).present?
   end
 
-  def politicians_sought?
+  def specific_politicians_sought?
     requested_location.present?
   end
 
   def sought_politicians
     @in_office = !params.has_key?(:in_office) || ['1', true].include?(params[:in_office])
-    if requested_location.present?
+    if !requested_location.nil?
       Politician.from(requested_location)
     elsif session[:geo_location]
       params[:representing] = session[:geo_location].full_address
@@ -20,9 +20,9 @@ module LocationsHelper
   end
 
   def requested_location
-    if params[:representing].present?
+    if params.has_key?(:representing)
       params[:representing]
-    elsif !params.has_key?(:representing) && session[:zip_code].present?
+    elsif session[:zip_code].present?
       session[:zip_code]
     end
   end
