29d2921c831617b566af6db221c65bad5adacbe4
Ben Woosley <ben.woosley@gmail.com>
2010-04-07 23:22:32 -0400

    Move Term#place into a term_place helper to support imminent sub-linking

--- app/helpers/politician_terms_helper.rb
+++ app/helpers/politician_terms_helper.rb
@@ -1,6 +1,20 @@
 module PoliticianTermsHelper
   def term_description(term)
     party = "; #{term.party}" if term.party
-    "#{term.title} #{term.place}#{party}"
+    "#{term.title} #{term_place(term)}#{party}"
+  end
+
+  def term_place(term)
+    case term
+    when PresidentialTerm
+      "of these United States"
+    when RepresentativeTerm
+      "for #{term.district.full_name}"
+    when SenateTerm
+      "for #{term.state.full_name}"
+    else
+      notify_exceptional("Unrecognized term #{term.inspect}")
+      ''
+    end
   end
 end
