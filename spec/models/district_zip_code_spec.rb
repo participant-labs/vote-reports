78339c96678420c47184d3336c889d2599f94ae8
Ben Woosley <ben.woosley@gmail.com>
2010-04-10 00:17:35 -0400

    Drop scaffolded spec

--- spec/models/district_zip_code_spec.rb
+++ spec/models/district_zip_code_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
 describe DistrictZipCode do
-  before(:each) do
-    @valid_attributes = {
-      :district_id => 1,
-      :zip_code => 1,
-      :plus_4 => 1
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    DistrictZipCode.create!(@valid_attributes)
-  end
 end
