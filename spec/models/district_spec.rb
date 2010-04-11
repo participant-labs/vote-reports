5c2008abdd8a971f446708cb33c5e30e527d235a
Ben Woosley <ben.woosley@gmail.com>
2010-04-10 21:23:03 -0400

    Test District.for_city a bit better

--- spec/models/district_spec.rb
+++ spec/models/district_spec.rb
@@ -1,4 +1,24 @@
 require 'spec_helper'
 
 describe District do
+  describe ".for_city" do
+    before do
+      @nm_district = create_district
+      @ca_district = create_district
+      @nm_district.zip_codes << create_zip_code(:zip_code => '11111')
+      @ca_district.zip_codes << create_zip_code(:zip_code => '22222')
+      create_location(:city => 'SAN FRANCISCO', :state => 'CA', :zip_code => '22222')
+      create_location(:city => 'SAN FRANCISCO', :state => 'NM', :zip_code => '11111')
+    end
+
+    it "should handle cities alone" do
+      District.for_city('san francisco').should =~ [@nm_district, @ca_district]
+      District.for_city('San francisco').should =~ [@nm_district, @ca_district]
+    end
+
+    it "should handle regular cities" do
+      District.for_city('san francisco, ca').should == [@ca_district]
+      District.for_city('san FRANCISCO, ca').should == [@ca_district]
+    end
+  end
 end
