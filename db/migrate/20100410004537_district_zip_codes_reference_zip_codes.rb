14f7ea5e0cb64bfa270d9af1c1096e1f32cec2b3
Ben Woosley <ben.woosley@gmail.com>
2010-04-11 11:13:47 -0400

    Fix district zip code migration

--- db/migrate/20100410004537_district_zip_codes_reference_zip_codes.rb
+++ db/migrate/20100410004537_district_zip_codes_reference_zip_codes.rb
@@ -1,12 +1,12 @@
 class DistrictZipCodesReferenceZipCodes < ActiveRecord::Migration
   def self.up
     $stdout.sync = true
+    rename_column :district_zip_codes, :zip_code, :zip_code_id
     DistrictZipCode.paginated_each do |district_zip_code|
-      zip_code = ZipCode.find_by_zip_code(district_zip_code[:zip_code]) || raise("Foo")
+      zip_code = ZipCode.find_by_zip_code(district_zip_code[:zip_code_id]) || raise("Foo")
       district_zip_code.update_attribute(:zip_code, zip_code)
       $stdout.print '.'
     end
-    rename_column :district_zip_codes, :zip_code, :zip_code_id
     constrain :district_zip_codes, :zip_code_id, :reference => {:zip_codes => :id}
   end
 
