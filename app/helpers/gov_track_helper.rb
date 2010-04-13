daef301df4a64b3f87fcee0561e0a7aca92bff16
Ben Woosley <ben.woosley@gmail.com>
2010-04-13 01:31:34 -0400

    Use the data-district attribute to attach maps to anything

--- app/helpers/gov_track_helper.rb
+++ app/helpers/gov_track_helper.rb
@@ -1,16 +1,19 @@
 module GovTrackHelper
-  def gov_track_map_url(object)
-    "http://www.govtrack.us/embed/mapframe.xpd?" +
-      if object.is_a?(UsState)
-        {:state => object.abbreviation}
-      elsif object.is_a?(District)
-        if object.district == 0
-          {:state => object.state.abbreviation}
-        else
-          {:state => object.state.abbreviation, :district => object.district}
-        end
+  def gov_track_map_params(object)
+    if object.is_a?(UsState)
+      {:state => object.abbreviation}
+    elsif object.is_a?(District)
+      if object.district == 0
+        {:state => object.state.abbreviation}
       else
-        raise "Not gov-trackable: #{object.inspect}"
-      end.to_param
+        {:state => object.state.abbreviation, :district => object.district}
+      end
+    else
+      raise "Not gov-trackable: #{object.inspect}"
+    end.to_param
+  end
+
+  def gov_track_map_url(object)
+    "http://www.govtrack.us/embed/mapframe.xpd?#{gov_track_map_params(object)}"
   end
 end
