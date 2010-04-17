35eed20d484070227c7276999f0470c1a6f4b689
Ben Woosley <ben.woosley@gmail.com>
2010-04-17 19:35:59 -0400

    Limit front-page example politicians to 5

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,7 +4,7 @@ class SiteController < ApplicationController
     @dont_show_geo_address = true
     params[:in_office] = true
 
-    @politicians = sought_politicians
+    @politicians = sought_politicians.scoped(:limit => 5)
 
     @recent_reports = Report.user_published.by_updated_at.all(:limit => 3)
     @featured_interest_group_reports = Report.interest_group_published.random.all(:limit => 3)
