158bd8e8f06160864426467283f45cb20226951a
Ben Woosley <ben.woosley@gmail.com>
2010-04-17 17:57:24 -0400

    Another attempt

--- app/controllers/reports/scores_controller.rb
+++ app/controllers/reports/scores_controller.rb
@@ -2,6 +2,6 @@ class Reports::ScoresController < ApplicationController
   def show
     @report = Report.find(params[:report_id])
     @score = @report.scores.find(params[:id])
-    render :action => 'show', :layout => false
+    render :layout => false
   end
 end
