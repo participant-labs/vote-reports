02bb9fe335847959ecb8b41a380d9b3b8bc90dd0
Ben Woosley <ben.woosley@gmail.com>
2010-04-17 22:03:56 -0400

    Don't show the same report twice on the front page

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,15 +4,16 @@ class SiteController < ApplicationController
     @dont_show_geo_address = true
     params[:in_office] = true
 
-    @politicians = sought_politicians.scoped(:limit => 5)
-    @scores =  @politicians.map {|politician| politician.report_scores.published.first(:order => 'random()') }.compact
-    if @scores.empty?
-      @politicians = Politician.in_office(true).scoped(:limit => 5)
-      @scores =  @politicians.map {|politician| politician.report_scores.published.first(:order => 'random()') }.compact
-    end
-
     @recent_reports = Report.user_published.by_updated_at.all(:limit => 3)
     @featured_interest_group_reports = Report.interest_group_published.random.all(:limit => 3)
+    reports = @recent_reports + @featured_interest_group_reports
+
+    @politicians = sought_politicians.scoped(:limit => 5)
+    @scores =  @politicians.map {|politician| politician.report_scores.published.first(:order => 'random()', :conditions => ['reports.id NOT IN(?)', reports]) }.compact
+    if @scores.empty?
+      @politicians = Politician.in_office(true).scoped(:limit => 5)
+      @scores =  @politicians.map {|politician| politician.report_scores.published.first(:order => 'random()', :conditions => ['reports.id NOT IN(?)', reports]) }.compact
+    end
   end
 
   def about
