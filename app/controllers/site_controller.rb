ddeaf4b7a1ac5f3e6a70ffca5e9494bdb0677dbc
Ben Woosley <ben.woosley@gmail.com>
2010-04-17 20:01:34 -0400

    If there aren't any scores for sought_politicians on the front page, return scores for random politicians

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -5,6 +5,11 @@ class SiteController < ApplicationController
     params[:in_office] = true
 
     @politicians = sought_politicians.scoped(:limit => 5)
+    @scores =  @politicians.map {|politician| politician.report_scores.published.first(:order => 'random()') }.compact
+    if @scores.empty?
+      @politicians = Politician.in_office(true).scoped(:limit => 5)
+      @scores =  @politicians.map {|politician| politician.report_scores.published.first(:order => 'random()') }.compact
+    end
 
     @recent_reports = Report.user_published.by_updated_at.all(:limit => 3)
     @featured_interest_group_reports = Report.interest_group_published.random.all(:limit => 3)
