a27b071070e2233232a4e53fa4c7cd9ee59b6aa6
Ben Woosley <ben.woosley@gmail.com>
2010-04-21 00:53:16 -0400

    Fix report image replacement

--- app/controllers/users/reports/thumbnails_controller.rb
+++ app/controllers/users/reports/thumbnails_controller.rb
@@ -1,6 +1,18 @@
 class Users::Reports::ThumbnailsController < ApplicationController
   filter_access_to :all
 
+  def create
+    @user = User.find(params[:user_id])
+    @report = @user.reports.find(params[:report_id], :scope => @user)
+    @report.build_image(params[:image])
+    if @report.save
+      flash[:notice] = "Successfully updated thumbnail."
+    else
+      flash[:error] = "Unable to update thumbnail."
+    end
+    redirect_to [@user, @report]
+  end
+
   def edit
     @user = User.find(params[:user_id])
     @report = @user.reports.find(params[:report_id], :scope => @user)
@@ -10,7 +22,7 @@ class Users::Reports::ThumbnailsController < ApplicationController
   def update
     @user = User.find(params[:user_id])
     @report = @user.reports.find(params[:report_id], :scope => @user)
-    if @report.thumbnail.update_attributes(params[:image])
+    if @report.image.update_attributes(params[:image])
       flash[:notice] = "Successfully updated thumbnail."
     else
       flash[:error] = "Unable to update thumbnail."
