3cb5189d2ea99044adb324b8a74af9342176f0e5
Ben Woosley <ben.woosley@gmail.com>
2010-04-25 19:21:09 -0400

    Drop bad scaffolded spec

--- spec/models/moderatorship_spec.rb
+++ spec/models/moderatorship_spec.rb
@@ -1,14 +1,4 @@
 require 'spec_helper'
 
 describe Moderatorship do
-  before(:each) do
-    @valid_attributes = {
-      :user_id => 1,
-      :created_by_id => 1
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    Moderatorship.create!(@valid_attributes)
-  end
 end
