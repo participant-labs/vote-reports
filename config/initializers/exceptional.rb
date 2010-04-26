efff8f31a44ff7b42bce62cc24d97697db2d7ec9
Ben Woosley <ben.woosley@gmail.com>
2010-04-25 21:47:35 -0400

    Exceptional.catch is private in newer versions of the gem

--- config/initializers/exceptional.rb
+++ config/initializers/exceptional.rb
@@ -1,7 +1,7 @@
 def notify_exceptional(exception)
   exception = RuntimeError.new(exception) unless exception.is_a?(Exception)
   Rails.logger.error(exception.inspect)
-  Exceptional.catch(exception) if defined?(Exceptional)
+  Exceptional.send(:catch, exception) if defined?(Exceptional)
   nil
 rescue => e
   Rails.logger.error("Trouble (#{e.inspect}) Notifying Exceptional of #{exception.inspect}")
