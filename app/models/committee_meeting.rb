1b5176cf611da3cfba2e3e9b1501ae14c29f805d
Ben Woosley <ben.woosley@gmail.com>
2010-04-27 01:02:13 -0400

    Add CommitteeMeeting#subcommittees for convenience (not yet realized)

--- app/models/committee_meeting.rb
+++ app/models/committee_meeting.rb
@@ -2,6 +2,15 @@ class CommitteeMeeting < ActiveRecord::Base
   belongs_to :committee
   belongs_to :congress
 
+  def subcommittees
+    CommitteeMeeting.scoped(
+      :conditions => {
+        :committee_id => committee.subcommittees,
+        :congress_id => congress_id
+      }
+    )
+  end
+
   has_many :memberships, :class_name => 'CommitteeMembership'
   has_many :members, :through => :memberships, :source => :politician
 
