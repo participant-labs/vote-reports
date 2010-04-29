a2a212d4ddb1cfcb260e3b73d9a4ce96b773ca41
Ben Woosley <ben.woosley@gmail.com>
2010-04-29 01:42:37 -0400

    Fix pull in old bills migration

--- db/migrate/20100429021806_pull_in_old_bills.rb
+++ db/migrate/20100429021806_pull_in_old_bills.rb
@@ -1,8 +1,8 @@
 class PullInOldBills < ActiveRecord::Migration
   def self.up
-    # Rake::Task['gov_track:download_all'].invoke
-    # Rake::Task['gov_track:politicians:unpack'].invoke
-    # Rake::Task['gov_track:committees:unpack'].invoke
+    Rake::Task['gov_track:download_all'].invoke
+    Rake::Task['gov_track:politicians:unpack'].invoke
+    Rake::Task['gov_track:committees:unpack'].invoke
     ENV['MEETING'] = '101,102'
     Rake::Task['gov_track:bills:unpack'].invoke
   end
