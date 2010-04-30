f814938b1316340c8dfd3592c9a15a7dfc38f7ed
Ben Woosley <ben.woosley@gmail.com>
2010-04-30 16:44:47 -0400

    Add people and committee update to the new bill update migration, as it was failing on politicians who had not been imported

--- db/migrate/20100429050816_update_latest_bills.rb
+++ db/migrate/20100429050816_update_latest_bills.rb
@@ -1,5 +1,8 @@
 class UpdateLatestBills < ActiveRecord::Migration
   def self.up
+    Rake::Task['gov_track:download_all'].invoke
+    Rake::Task['gov_track:politicians:unpack'].invoke
+    Rake::Task['gov_track:committees:unpack'].invoke
     ENV['MEETING'] = '111'
     ENV['UPDATE'] = 'true'
     Rake::Task['gov_track:bills:unpack'].invoke
