694c9b45e2c06ef415940996bbbfcc1a1832a917
Ben Woosley <ben.woosley@gmail.com>
2010-04-30 15:40:04 -0400

    Add a migration for adding in the amendments from 101 & 102

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100429050816) do
+ActiveRecord::Schema.define(:version => 20100430192743) do
 
   create_table "adminships", :force => true do |t|
     t.integer  "user_id",       :null => false
