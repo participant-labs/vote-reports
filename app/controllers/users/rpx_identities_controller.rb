7399e71c4bb55345d9ac745567649faa431c0d5e
Ben Woosley <ben.woosley@gmail.com>
2010-05-02 04:14:55 -0400

    Deleting/creating rpx_identifiers should be managable for an admin

--- app/controllers/users/rpx_identities_controller.rb
+++ app/controllers/users/rpx_identities_controller.rb
@@ -2,20 +2,20 @@ class Users::RpxIdentitiesController < ApplicationController
   filter_access_to :all
 
   def create
-    @user = current_user
+    @user = User.find(params[:user_id])
     if @user.save
       flash[:notice] = "Successfully added login to this account."
     else
       flash[:error] = "Unable to add login because it is already associated with another account"
     end
-    redirect_to user_path(current_user)
+    redirect_to user_path(@user)
   end
 
   def destroy
-    @user = current_user
+    @user = User.find(params[:user_id])
     @identifier = @user.rpx_identifiers.find(params[:id])
     @identifier.destroy
     flash[:notice] = "Successfully removed login."
-    redirect_to edit_user_path(current_user)
+    redirect_to edit_user_path(@user)
   end
 end
