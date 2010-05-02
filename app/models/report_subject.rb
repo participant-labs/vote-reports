6f9289cec6e47aa18eb2c9595a16a187fa836b74
Ben Woosley <ben.woosley@gmail.com>
2010-05-02 02:50:26 -0400

    Move the bulk of the ReportSubject generate to a method called ReportSubjec.generate_for, which is report-specific, for reuse

--- app/models/report_subject.rb
+++ app/models/report_subject.rb
@@ -7,38 +7,40 @@ class ReportSubject < ActiveRecord::Base
   # [report, subject] uniqueness
 
   class << self
+    def generate_for(report)
+      bill_criteria_subjects = report.bill_criteria_subjects.scoped(
+        :select => "DISTINCT(subjects.id), COUNT(subjects.id) AS count",
+        :group => 'subjects.id').inject({}) do |hash, subject|
+        hash[subject] = subject.count
+        hash
+      end
+
+      if report.interest_group
+        count = report.interest_group.reports.count
+        report.interest_group.subjects.each do |subject|
+          bill_criteria_subjects[subject] ||= 0
+          bill_criteria_subjects[subject] += count
+        end
+      end
+
+      if bill_criteria_subjects.present?
+        ReportSubject.import_without_validations_or_callbacks(
+          [:report_id, :subject_id, :count],
+          bill_criteria_subjects.map do |(subject, count)|
+            [report.id, subject.id, count]
+          end
+        )
+      end
+    end
+
     def generate!
       require 'ar-extensions'
       require 'ar-extensions/import/postgresql'
 
       transaction do
         delete_all
-        subjects = []
-        Report.paginated_each do |report|
-          bill_criteria_subjects = report.bill_criteria_subjects.scoped(
-            :select => "DISTINCT(subjects.id), COUNT(subjects.id) AS count",
-            :group => 'subjects.id').inject({}) do |hash, subject|
-            hash[subject] = subject.count
-            hash
-          end
-
-          if report.interest_group
-            count = report.interest_group.reports.count
-            report.interest_group.subjects.each do |subject|
-              bill_criteria_subjects[subject] ||= 0
-              bill_criteria_subjects[subject] += count
-            end
-          end
-
-          subjects += bill_criteria_subjects.map do |(subject, count)|
-            [report.id, subject.id, count]
-          end
-        end
-        if subjects.present?
-          ReportSubject.import_without_validations_or_callbacks(
-            [:report_id, :subject_id, :count],
-            subjects
-          )
+        paginated_each do |report|
+          generate_for(report)
         end
       end
     end
