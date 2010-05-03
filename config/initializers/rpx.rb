fa5b8c866c7556d3bffd7d4c74b31b2fb7a73dc0
Ben Woosley <ben.woosley@gmail.com>
2010-05-02 22:03:08 -0400

    Move RPXIdentifier extension into an initializer, so it's guaranteed to be called (rather than loaded on demand)

--- config/initializers/rpx.rb
+++ config/initializers/rpx.rb
@@ -3,5 +3,9 @@ begin
 rescue LoadError
 end
 
+class RPXIdentifier < ActiveRecord::Base
+  belongs_to :user
+end
+
 RPX_API_KEY = ENV['RPX_API_KEY'] unless defined?(RPX_API_KEY)
 RPX_APP_NAME = 'VoteReports'
