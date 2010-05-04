b78665d331f2ee550789ebfbf7c56a683e13efe2
Ben Woosley <ben.woosley@gmail.com>
2010-05-04 04:16:52 -0400

    Give some order to the Report Bill Criteria page, and rename it the Agenda

--- app/helpers/bill_criteria_helper.rb
+++ app/helpers/bill_criteria_helper.rb
@@ -8,6 +8,12 @@ module BillCriteriaHelper
     end
   end
 
+  def bill_criterion_evidence(criterion)
+    if criterion.explanatory_url.present?
+      link_to 'Evidence', criterion.explanatory_url
+    end
+  end
+
   def bill_criterion_status(criterion)
     if criterion.unvoted?
       content_tag :span, criterion.bill.congress.current? ? "(no votes yet)" : "(no votes)", :class => 'info', :title => 'We have no record of roll call votes for this bill'
