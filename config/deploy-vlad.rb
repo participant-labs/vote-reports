6596684f3eb4639bfdb7bf9cdf086e9241401799
Ben Woosley <ben.woosley@gmail.com>
2010-05-06 02:28:14 -0400

    Drop vlad lines which are covered by default in moonshine

--- config/deploy-vlad.rb
+++ config/deploy-vlad.rb
@@ -5,27 +5,13 @@ set :deploy_to, "/var/www/votereports/production/"
 set :repository, 'git@github.com:Empact/vote-reports.git'
 
 namespace :vlad do
-  desc "custom deploy"
-  task :update_symlinks => [:internal_symlinks]
-  task :deploy => [:update, :install_gems, :migrate, :setup_scheduling, :start_dj, :setup_assets, :start]
-
-  set :rake_cmd, 'nice -n 3 rake'
-  set :web_command, "apache2ctl"
-
   remote_task :internal_symlinks, :roles => :app do
     run [
-      "ln -s #{latest_release}/config/database.rimu.yml #{latest_release}/config/database.yml",
       "ln -s #{shared_path}/data #{latest_release}/data",
-      "ln -s #{shared_path}/assets #{latest_release}/public/assets",
       "ln -s #{shared_path}/config/secure_variables.rb #{latest_release}/config/secure_variables.rb",
       "ln -fs /usr/local/bin/passenger-memory-stats /usr/bin/passenger-memory-stats",
-      "cp -f #{latest_release}/config/deploy/log_rotation.conf /etc/logrotate.d/votereports",
       "chown -R www-data:www-data #{latest_release}/public/assets"
     ].join(' && ')
-  end
-
-  remote_task :install_gems, :roles => :app do
-    run "cd #{latest_release} && rake gems:install"
   end
 
   remote_task :start_dj, :roles => :app do
