669ba301e6edd9e499ed978b0d7879893ea1bda1
Ben Woosley <ben.woosley@gmail.com>
2010-05-07 02:32:29 -0400

    Really, we should be focusing on staging

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,5 +2,5 @@ set :stages, %w(staging production)
 set :default_stage, "staging"
 require 'capistrano/ext/multistage'
 
-server 'votereports.org', :app, :web, :db, :primary => true
+# server 'votereports.org', :app, :web, :db, :primary => true
 server 'staging.votereports.org', :app, :web, :db, :primary => true
