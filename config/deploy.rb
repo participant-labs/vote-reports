05635d1b1db4351a33328333af1b0007d005e48b
Ben Woosley <ben.woosley@gmail.com>
2010-05-07 03:56:15 -0400

    capistrano-ext seems to have caused trouble

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,6 +1,2 @@
-set :stages, %w(staging production)
-set :default_stage, "staging"
-require 'capistrano/ext/multistage'
-
 # server 'votereports.org', :app, :web, :db, :primary => true
 server 'staging.votereports.org', :app, :web, :db, :primary => true
