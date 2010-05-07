73505905278ccf21525d820c5cb909efaa47f0a7
Ben Woosley <ben.woosley@gmail.com>
2010-05-07 02:30:52 -0400

    Add multistage

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,1 +1,6 @@
-server 'yourapp.com', :app, :web, :db, :primary => true
+set :stages, %w(staging production)
+set :default_stage, "staging"
+require 'capistrano/ext/multistage'
+
+server 'votereports.org', :app, :web, :db, :primary => true
+server 'staging.votereports.org', :app, :web, :db, :primary => true
