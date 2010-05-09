a92b77e62c726e25a9c2cf3ab07f1d3a5d630971
Ben Woosley <ben.woosley@gmail.com>
2010-05-09 16:08:47 -0400

    Add deploy strategy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,2 +1,9 @@
+set :user, 'deploy'
+
 # server 'votereports.org', :app, :web, :db, :primary => true
 server 'staging.votereports.org', :app, :web, :db, :primary => true
+
+set :deploy_via, :remote_cache
+set :copy_cache, true
+set :copy_exclude, [".git"]
+set :use_sudo, false
