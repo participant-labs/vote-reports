a70cf70161d94c9d0920e77fc1949c113e50dae7
Ben Woosley <ben.woosley@gmail.com>
2010-05-11 04:18:46 -0400

    Have the front-page zip code replace the visible scores

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,6 +4,10 @@ class SiteController < ApplicationController
     @dont_show_geo_address = true
     params[:in_office] = true
 
+    if params[:representing].present? && zip_code?(params[:representing])
+      session[:zip_code] = params[:representing]
+    end
+
     @recent_reports = Report.user_published.by_updated_at.all(:limit => 3)
     @featured_interest_group_reports = Report.interest_group_published.random.all(:limit => 3)
     reports = @recent_reports + @featured_interest_group_reports
@@ -13,6 +17,13 @@ class SiteController < ApplicationController
     if @scores.empty?
       @politicians = Politician.in_office(true).scoped(:limit => 5)
       @scores =  @politicians.map {|politician| politician.report_scores.published.first(:order => 'random()', :conditions => ['reports.id NOT IN(?)', reports]) }.compact
+    end
+
+    respond_to do |format|
+      format.html
+      format.js {
+        render :partial => 'scores/list', :locals => {:scores => @scores}
+      }
     end
   end
 
