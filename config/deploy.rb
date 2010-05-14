45c3661ac0b012079492c3bc47d776cbc5698260
Ben Woosley <ben.woosley@gmail.com>
2010-05-14 18:53:55 -0400

    Handle delayed_job restarting from capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -8,3 +8,8 @@ set :deploy_via, :remote_cache
 set :copy_cache, true
 set :copy_exclude, [".git"]
 set :use_sudo, false
+
+require 'delayed/recipes'
+after "deploy:start", "delayed_job:start"
+after "deploy:stop", "delayed_job:stop"
+after "deploy:restart", "delayed_job:restart"
