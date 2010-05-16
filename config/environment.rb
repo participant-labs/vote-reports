d1f9f43b7f3c0cd3dc1d2c9852cd3006885c2e0d
Ben Woosley <ben.woosley@gmail.com>
2010-05-16 14:47:08 -0400

    bson_ext isn't loaded, I don't think

--- config/environment.rb
+++ config/environment.rb
@@ -32,7 +32,7 @@ Rails::Initializer.run do |config|
   config.gem 'jammit'
   config.gem 'json', :version => '>= 1.4.3'
   config.gem 'shadow_puppet'
-  config.gem 'bson_ext', :version => '>= 1.0.1'
+  config.gem 'bson_ext', :version => '>= 1.0.1', :lib => false
   config.gem 'mongo_mapper', :version => '>= 0.7.5'
   config.gem 'newrelic_rpm', :version => '>= 2.11.2'
   config.gem 'nokogiri'
