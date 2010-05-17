ee0a01ea5b795ca650a8a769c779d0db15c0702a
Ben Woosley <ben.woosley@gmail.com>
2010-05-17 01:07:56 -0400

    Proper chart subtitle via extracted score_evidence_components

--- app/helpers/reports/scores_helper.rb
+++ app/helpers/reports/scores_helper.rb
@@ -3,4 +3,10 @@ module Reports::ScoresHelper
     by = " by #{score.report.owner}" unless score.report.interest_group_id
     "#{score} for #{score.politician.full_name} on '#{score.report.name}' #{by}"
   end
+
+  def score_evidence_components(score)
+    score.evidence.group_by(&:evidence_type).map do |(type, evidence)|
+      pluralize(evidence.size, human_type_name(type))
+    end
+  end
 end
