6c3d290d26a6e79ddcaee2c0f42631c49d9494f9
Ben Woosley <ben.woosley@gmail.com>
2010-05-22 15:55:03 -0400

    Update mongo_mapper

--- config/environment.rb
+++ config/environment.rb
@@ -35,7 +35,7 @@ Rails::Initializer.run do |config|
   config.gem 'bson_ext', :version => '= 1.0.0', :lib => false
   config.gem 'bson', :version => '= 1.0.0'
   config.gem 'mongo', :version => '= 1.0.0'
-  config.gem 'mongo_mapper', :version => '>= 0.7.5'
+  config.gem 'mongo_mapper', :version => '>= 0.7.6'
   config.gem 'newrelic_rpm', :version => '>= 2.11.2'
   config.gem 'nokogiri'
   config.gem 'paperclip'
