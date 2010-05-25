c6172b57c46829df076d966cefd27926e4cdef49
Ben Woosley <ben.woosley@gmail.com>
2010-05-25 00:04:43 -0400

    Update Cucumber to 0.3.1

--- features/support/paths.rb
+++ features/support/paths.rb
@@ -62,14 +62,20 @@ module NavigationHelpers
       send("#{$1.gsub(' ', '_')}_path")
 
     # Add more mappings here.
-    # Here is a more fancy example:
+    # Here is an example that pulls values out of the Regexp:
     #
     #   when /^(.*)'s profile page$/i
     #     user_profile_path(User.find_by_login($1))
 
     else
-      raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
-        "Now, go and add a mapping in #{__FILE__}"
+      begin
+        page_name =~ /the (.*) page/
+        path_components = $1.split(/\s+/)
+        self.send(path_components.push('path').join('_').to_sym)
+      rescue Object => e
+        raise "Can't find mapping from \"#{page_name}\" to a path.\n" +
+          "Now, go and add a mapping in #{__FILE__}"
+      end
     end
   end
 end
