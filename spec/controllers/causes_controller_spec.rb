aeaf483952c1f58f319a17a442c10581b14c331c
Ben Woosley <ben.woosley@gmail.com>
2010-05-27 00:16:57 -0400

    Add cause index and show, linked from the header

---
+++ spec/controllers/causes_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe CausesController do
+
+  #Delete this example and add some real ones
+  it "should use CausesController" do
+    controller.should be_an_instance_of(CausesController)
+  end
+
+end
