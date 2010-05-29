cf56f8edea0737d0c50f25d640909ab0becc8c98
Ben Woosley <ben.woosley@gmail.com>
2010-05-29 17:59:31 -0400

    Constrain scores to being within 0 and 100

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100529203141) do
+ActiveRecord::Schema.define(:version => 20100529203818) do
 
   create_table "adminships", :force => true do |t|
     t.integer  "user_id",       :null => false
