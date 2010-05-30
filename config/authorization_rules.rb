a33fec3b5c9adbaaadb9326c7ce22f65d9406451
Ben Woosley <ben.woosley@gmail.com>
2010-05-29 20:32:19 -0400

    Drop unnecessary [] in auth rules

--- config/authorization_rules.rb
+++ config/authorization_rules.rb
@@ -1,9 +1,9 @@
 authorization do
   role :admin do
     includes :moderator
-    has_permission_on [:causes, :causes_reports], :to => [:new, :create, :edit, :update, :destroy]
-    has_permission_on [:users, :users_reports, :reports], :to => [:index, :show, :edit, :update, :destroy]
-    has_permission_on [:users_adminships, :users_moderatorships], :to => [:create, :destroy]
+    has_permission_on :causes, :causes_reports, :to => [:new, :create, :edit, :update, :destroy]
+    has_permission_on :users, :users_reports, :reports, :to => [:index, :show, :edit, :update, :destroy]
+    has_permission_on :users_adminships, :users_moderatorships, :to => [:create, :destroy]
   end
 
   role :moderator do
@@ -20,7 +20,7 @@ authorization do
     has_permission_on :users_rpx_identities, :to => [:create, :destroy] do
       if_attribute :user_id => is {user.id}
     end
-    has_permission_on [:reports, :users_reports, :users_reports_bill_criteria, :users_reports_thumbnails], :to => [:show, :index, :new, :create, :edit, :update, :destroy] do
+    has_permission_on :reports, :users_reports, :users_reports_bill_criteria, :users_reports_thumbnails, :to => [:show, :index, :new, :create, :edit, :update, :destroy] do
       if_attribute :user_id => is {user.id}
     end
     has_permission_on [:reports, :users_reports], :to => :show do
@@ -30,13 +30,13 @@ authorization do
 
   role :guest do
     includes :all
-    has_permission_on [:users, :user_sessions], :to => [:new, :create]
+    has_permission_on :users, :user_sessions, :to => [:new, :create]
   end
 
   role :all do
     has_permission_on :causes, :causes_reports, :causes_scores, :to => [:index, :show]
-    has_permission_on [:reports, :users_reports], :to => :index
-    has_permission_on [:reports, :users_reports], :to => :show do
+    has_permission_on :reports, :users_reports, :to => :index
+    has_permission_on :reports, :users_reports, :to => :show do
       if_attribute :state => ['published', 'personal']
     end
   end
