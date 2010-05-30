1e5314d8e816d35b6d0bcbc8f01e975b08d20c3e
Ben Woosley <ben.woosley@gmail.com>
2010-05-29 23:20:46 -0400

    Ensure a Cause always has a #report present

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -3,16 +3,21 @@ class Cause < ActiveRecord::Base
   has_many :reports, :through => :cause_reports
 
   has_one :report
+  before_validation_on_create :initialize_report
 
   accepts_nested_attributes_for :cause_reports, :reject_if => proc {|attributes| attributes['support'] == '0' }
 
   has_friendly_id :name, :use_slug => true
 
-  validates_presence_of :name, :description
+  validates_presence_of :name, :description, :report
 
-  def rescore!
-    (report || build_report(:name => name).tap(&:save!)).rescore!
-  end
+  delegate :rescore!, :to => :report
 
   alias_method :score_criteria, :cause_reports
+
+  private
+
+  def initialize_report
+    build_report(:name => name)
+  end
 end
