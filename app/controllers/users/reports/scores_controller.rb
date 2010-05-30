d3ad7de87239681852c1427afad22d03847cee20
Ben Woosley <ben.woosley@gmail.com>
2010-05-30 17:25:34 -0400

    Make report scores tab ajaxy

--- app/controllers/users/reports/scores_controller.rb
+++ app/controllers/users/reports/scores_controller.rb
@@ -1,8 +1,27 @@
 class Users::Reports::ScoresController < ApplicationController
+  before_filter :load_report
+
+  def index
+    @scores = @report.scores.for_politicians(sought_politicians)
+    respond_to do |format|
+      format.html {
+        render :layout => false
+      }
+      format.js {
+        render :partial => 'reports/scores/table', :locals => {:report => @report, :scores => @scores}
+      }
+    end
+  end
+
   def show
-    @user = User.find(params[:user_id])
-    @report = @user.reports.find(params[:report_id], :scope => @user)
     @score = @report.scores.find(params[:id])
     render :layout => false
+  end
+
+  private
+
+  def load_report
+    @user = User.find(params[:user_id])
+    @report = @user.reports.find(params[:report_id], :scope => @user)
   end
 end
---
+++ spec/controllers/users/reports/scores_controller_spec.rb
@@ -0,0 +1,4 @@
+require 'spec_helper'
+
+describe Users::Reports::ScoresController do
+end
