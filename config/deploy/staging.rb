d535b98c1d5a875d29cce115777f7e7615f6c394
Ben Woosley <ben.woosley@gmail.com>
2010-06-05 02:00:08 -0400

    Final (I think) solr fixes for staging

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@ server domain, :app, :web, :db, :primary => true
 namespace :sunspot do
   desc "Update the crontab file"
   task :start, :roles => :app do
-    run "cd #{current_path} && rake sunspot:start RAILS_ENV=staging"
+    run "cd #{current_path} && rake sunspot:start"
   end
   after "deploy:symlink", "sunspot:start"
 end
