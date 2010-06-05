3a04985287ad0a8b17b797d213dd41a13db3f5d1
Ben Woosley <ben.woosley@gmail.com>
2010-06-05 01:22:18 -0400

    Add a cap task to try to run solr on staging via the rake task

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,3 +2,11 @@ set :domain, 'staging.votereports.org'
 set :rails_env, "staging"
 
 server domain, :app, :web, :db, :primary => true
+
+namespace :sunspot do
+  desc "Update the crontab file"
+  task :start, :roles => :app do
+    run "cd #{release_path} && rake sunspot:solr:start RAILS_ENV=staging"
+  end
+  after "deploy:symlink", "sunspot:start"
+end
