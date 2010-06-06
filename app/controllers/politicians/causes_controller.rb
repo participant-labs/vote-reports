dfd7d366cd47110bd1a19907558da87c4bfa5793
Ben Woosley <ben.woosley@gmail.com>
2010-06-05 21:27:25 -0400

    Narrow the politician causes pagionation update area

--- app/controllers/politicians/causes_controller.rb
+++ app/controllers/politicians/causes_controller.rb
@@ -2,6 +2,13 @@ class Politicians::CausesController < ApplicationController
   def index
     @politician = Politician.find(params[:politician_id])
     @scores = @politician.report_scores.for_causes.paginate :page => params[:page]
-    render :layout => false
+    respond_to do |format|
+      format.html {
+        render :layout => false
+      }
+      format.js {
+        render :partial => 'politicians/scores/table',
+          :locals => {:scores => @scores, :replace => 'report_scores', :source => 'Cause'}
+      }
   end
 end
