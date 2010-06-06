28f21c27fde74da97d7f0eb015c4f36a08449026
Ben Woosley <ben.woosley@gmail.com>
2010-06-05 22:28:14 -0400

    Revert "Extract HasReport from Cause and InterestGroup"
    This reverts commit e3b51b04070be0e602a7ea3ee25924e80717be08.

--- app/models/cause.rb
+++ app/models/cause.rb
@@ -1,14 +1,16 @@
 class Cause < ActiveRecord::Base
-  include HasReport
-
   has_many :cause_reports
   has_many :reports, :through => :cause_reports
+
+  has_one :report
+  before_validation_on_create :initialize_report
+  after_update :update_report
 
   accepts_nested_attributes_for :cause_reports, :reject_if => proc {|attributes| attributes['support'] == '0' }
 
   has_friendly_id :name, :use_slug => true
 
-  validates_presence_of :description
+  validates_presence_of :name, :description, :report
 
   delegate :rescore!, :to => :report
 
@@ -18,5 +20,17 @@ class Cause < ActiveRecord::Base
     def per_page
       20
     end
+  end
+
+  private
+
+  def update_report
+    if name_changed? || description_changed?
+      report.update_attributes(:name => name, :description => description)
+    end
+  end
+
+  def initialize_report
+    build_report(:name => name)
   end
 end
