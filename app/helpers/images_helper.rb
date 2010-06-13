3bd0f371fb624ebcc361d85b0944e28c76a96df7
Ben Woosley <ben.woosley@gmail.com>
2010-06-12 23:37:02 -0400

    Fix that image_with_hover_tag wasn't getting hovers

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -14,7 +14,7 @@ module ImagesHelper
   def image_with_hover_tag(image, title, attrs = {})
     style = attrs.delete(:style) || image.thumbnail.default_style
     attrs.reverse_merge(:alt => title, :title => title)
-    attrs.merge(hover_image_attrs(image)) if style != :large
+    attrs.merge!(hover_image_attrs(image)) if style != :large
     image_tag(image.thumbnail.url(style), attrs)
   end
 end
