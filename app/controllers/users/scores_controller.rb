fb027c6b15c3f051c43b93e16acbd89074ad3531
Ben Woosley <ben.woosley@gmail.com>
2010-06-14 02:31:24 -0400

    Fix location searching for personal report scores

--- app/controllers/users/scores_controller.rb
+++ app/controllers/users/scores_controller.rb
@@ -2,7 +2,7 @@ class Users::ScoresController < ApplicationController
   def index
     @user = User.find(params[:user_id])
     @report = @user.personal_report
-    @scores = @report.scores
+    @scores = @report.scores.for_politicians(sought_politicians)
 
     respond_to do |format|
       format.html
