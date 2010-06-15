613b21698c1927ecbb843c2d2ae23d18458b5e52
Ben Woosley <ben.woosley@gmail.com>
2010-06-14 23:02:54 -0400

    Move guides/show to guides/new to reflect that there will be a guide object backing the guide

--- config/routes.rb
+++ config/routes.rb
@@ -40,7 +40,7 @@ ActionController::Routing::Routes.draw do |map|
   map.resources :rolls, :only => [:show]
 
   map.resource :location
-  map.resource :guide
+  map.resources :guides
 
   map.resources :us_states, :as => 'us/states', :controller => 'us/states', :only => 'show' do |state|
     state.resource :map, :controller => 'us/states/maps', :only => 'show'
