c8fd1f3ab189e866f6c7a494e5a82abcd1841608
Ben Woosley <ben.woosley@gmail.com>
2010-06-14 22:39:32 -0400

    Start moving guide over to causes

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -6,23 +6,10 @@ class GuidesController < ApplicationController
       session[:zip_code] = params[:representing]
     end
 
-    params[:subjects] ||= []
     params[:in_office] = true if params[:in_office].nil?
-
     @politicians = sought_politicians.all(:limit => 5)
-    @topical_reports = Report.published.with_scores_for(@politicians)
-    if params[:subjects].present?
-      @topical_reports = @topical_reports.with_subjects(params[:subjects])
-    end
 
-    @subjects =
-      if @topical_reports
-        Subject.for_report(@topical_reports)
-      else
-        Subject.on_published_reports
-      end.for_tag_cloud.all(:limit => 20)
-
-    @topical_reports = @topical_reports.all(:limit => 10) if @topical_reports
+    @causes = Cause.all(:order => 'name')
 
     respond_to do |format|
       format.html
