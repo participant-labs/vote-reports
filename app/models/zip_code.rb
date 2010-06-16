68ee8030b34e6ac4e5c5320025a338f500c0cd56
Ben Woosley <ben.woosley@gmail.com>
2010-06-16 11:51:22 -0400

    Refactor some zip_code stuff into the zip_code model

--- app/models/zip_code.rb
+++ app/models/zip_code.rb
@@ -2,4 +2,18 @@ class ZipCode < ActiveRecord::Base
   has_many :congressional_district_zip_codes
   has_many :congressional_districts, :through => :congressional_district_zip_codes
   has_many :locations
+
+  class << self
+    def sections_of(zip_code)
+      zip_code.to_s.match(/^[^\d]*(\d{5})[-\s]*(\d{0,4})?\s*$/).try(:captures)
+    end
+
+    def zip_code?(location)
+      sections_of(location).present?
+    end
+  end
+
+  def to_s
+    zip_code.to_s
+  end
 end
