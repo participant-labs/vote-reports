d442e3dc98f6fd4c5f0bc25557f012e44c51c9a9
Ben Woosley <ben.woosley@gmail.com>
2010-06-16 14:36:20 -0400

    Normalize zip codes as we bring them in

--- spec/models/zip_code_spec.rb
+++ spec/models/zip_code_spec.rb
@@ -1,13 +1,26 @@
 require 'spec_helper'
 
 describe ZipCode do
-  before(:each) do
-    @valid_attributes = {
-      :zip_code => 1
-    }
-  end
+  describe ".zip_code" do
+    it "should recognize regular zip codes" do
+      ZipCode.zip_code('75028').should == '75028'
+      ZipCode.zip_code(' 75028').should == '75028'
+      ZipCode.zip_code('75028  ').should == '75028'
+    end
 
-  it "should create a new instance given valid attributes" do
-    ZipCode.create!(@valid_attributes)
+    it "should recognize regular zip code + 4" do
+      ZipCode.zip_code('75028-1111').should == '75028-1111'
+      ZipCode.zip_code('75028 1111').should == '75028-1111'
+      ZipCode.zip_code('750281111').should == '75028-1111'
+      ZipCode.zip_code('  750281111 ').should == '75028-1111'
+    end
+
+    it "should not recognize non zips" do
+      ZipCode.zip_code('75025-111').should == nil
+      ZipCode.zip_code('TX').should == nil
+      ZipCode.zip_code('7502').should == nil
+      ZipCode.zip_code('7502').should == nil
+      ZipCode.zip_code('7502-1111').should == nil
+    end
   end
 end
