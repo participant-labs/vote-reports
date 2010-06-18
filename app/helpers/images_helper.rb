8ab836f1c800d52d14d82cda091371a09daa840f
Ben Woosley <ben.woosley@gmail.com>
2010-06-18 01:12:41 -0400

    Fix that hover images were getting the wrong tities

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -13,7 +13,7 @@ module ImagesHelper
 
   def image_with_hover_tag(image, title, attrs = {})
     style = attrs.delete(:style) || image.thumbnail.default_style
-    attrs.reverse_merge(:alt => title, :title => title)
+    attrs.reverse_merge!(:alt => title, :title => title)
     attrs.merge!(hover_image_attrs(image)) if style != :large
     image_tag(image.thumbnail.url(style), attrs)
   end
