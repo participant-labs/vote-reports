7a24ecc79dc2dfb16eda6ebccdfd4a5f3cf17b86
Ben Woosley <ben.woosley@gmail.com>
2010-06-19 02:33:14 -0400

    Drop the last remaining data-qtip-path, bill titles

--- public/javascripts/qtip.js
+++ public/javascripts/qtip.js
@@ -1,47 +1,5 @@
 ;(function($) {
   $(document).ready(function(){
-    $('[data-qtip-path]').live('mouseover', function(event) {
-      var self = $(event.target).closest('[data-qtip-path]');
-      if (!self.data('init')) {
-        self.data('init', true);
-        var title = self.attr('data-qtip-title');
-        self.qtip({
-          content: {
-            url: self.attr('data-qtip-path'),
-            title: { text: title }
-          },
-          position: {
-            corner: {
-              target: 'bottomMiddle',
-              tooltip: 'topMiddle'
-            }
-          },
-          style: {
-            border: {
-              width: 5,
-              radius: 5,
-              color: '#8f8f8f'
-            },
-            tip: {
-              corner: 'topMiddle',
-              size: { x: 18, y: 18 }
-            },
-            width: 450
-          },
-          show: {
-            effect: { length: 200 },
-            delay: self.attr('data-qtip-delay') || 140
-          },
-          hide: {
-            fixed: true,
-            delay: 500,
-            effect: { length: 500 }
-         }
-        });
-        self.mouseover();
-      }
-    });
-
     $('[data-qtip-image]').live('mouseover', function(event) {
       var self = $(event.target).closest('[data-qtip-image]');
       if (!self.data('init')) {
