c9315005c55854880891c524c77e1bcb049307aa
Ben Woosley <ben.woosley@gmail.com>
2010-06-21 00:21:26 -0400

    Clean up district names ala mcommons

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100621041028) do
+ActiveRecord::Schema.define(:version => 20100621042016) do
 
   create_table "adminships", :force => true do |t|
     t.integer  "user_id",       :null => false
