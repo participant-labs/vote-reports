164aef6fb5dd1f04517bf6cf0450fe298c954312
Ben Woosley <ben.woosley@gmail.com>
2010-06-21 16:39:40 -0400

    Add html5 and google gears geolocation

---
+++ public/javascripts/geolocation.js
@@ -0,0 +1,24 @@
+;(function($) {
+  $(function() {
+
+    if(location_set) {
+    } else if(navigator.geolocation) {
+      // Try W3C Geolocation (Preferred)
+      navigator.geolocation.getCurrentPosition(function(position) {
+        $.post('location', {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
+        });
+      });
+    } else if (google.gears) {
+      // Try Google Gears Geolocation
+      var geo = google.gears.factory.create('beta.geolocation');
+      geo.getCurrentPosition(function(position) {
+        $.post('location', {
+          lat: position.latitude,
+          lng: position.longitude
+        });
+      });
+    }
+  });
+})(jQuery);
