90d554e823fb75f408ece3f5cbf64a1606f633fe
Ben Woosley <ben.woosley@gmail.com>
2010-06-21 22:15:08 -0400

    Use data-replace for location updates

--- app/controllers/locations_controller.rb
+++ app/controllers/locations_controller.rb
@@ -10,26 +10,45 @@ class LocationsController < ApplicationController
 
   def create
     @geoloc = Geokit::Geocoders::MultiGeocoder.geocode(params[:location])
-    if !@geoloc.success?
-      flash.now[:error] = %Q{Sorry, we were unable to understand this location. Could you clarify it?}
-      render :action => 'new'
-    elsif !@geoloc.is_us?
-      flash.now[:error] = %Q{Sorry, we currently only handle representatives for the United States of America.}
-      render :action => 'new'
-    else
-      if !%w[street address].include?(@geoloc.precision)
-        flash.now[:notice] = %Q{For more accurate results, an address or intersection is best, but here's our best guess from what you've said.}
+    action =
+      if !@geoloc.success?
+        flash.now[:error] = %Q{Sorry, we were unable to understand this location. Could you clarify it?}
+        'new'
+      elsif !@geoloc.is_us?
+        flash.now[:error] = %Q{Sorry, we currently only handle representatives for the United States of America.}
+        'new'
       else
-        flash.now[:success] = "Successfully set location"
-      end
-      session[:location] = params[:location]
-      session[:geo_location] = @geoloc
+        if !%w[street address].include?(@geoloc.precision)
+          flash.now[:notice] = %Q{For more accurate results, an address or intersection is best, but here's our best guess from what you've said.}
+        else
+          flash.now[:success] = "Successfully set location"
+        end
+        session[:location] = params[:location]
+        session[:geo_location] = @geoloc
 
-      redirect_to :action => 'show'
+        @districts = District.lookup(@geoloc)
+        @federal = @districts.federal.first
+        @bounds = @federal.polygon.envelope
+        @politicians = Politician.for_districts(@districts).in_office
+
+        'show'
+      end
+    respond_to do |format|
+      format.html {
+        render :action => action
+      }
+      format.js {
+        @js = true
+        render :action => action, :layout => false
+      }
     end
   end
 
   def show
+    if params[:location].present?
+      create
+      return
+    end
     unless @geoloc = session[:geo_location]
       render :action => :new
       return
