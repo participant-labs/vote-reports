a96e54f064edc4d5335eb5bf0198fbcf4f421e6d
Ben Woosley <ben.woosley@gmail.com>
2010-06-23 13:23:55 -0400

    Fix at-large district maps, which were lost because our districts table calls them district 1 (justifiably) rather than 0.

--- app/models/congressional_district.rb
+++ app/models/congressional_district.rb
@@ -64,7 +64,8 @@ class CongressionalDistrict < ActiveRecord::Base
   alias_method :full_name, :title
 
   def district_geometry
-    @district_geometry ||= District.federal.first(:conditions => {:us_state_id => us_state_id, :name => district.to_s})
+    @district_geometry ||= District.federal.first(:conditions => {
+      :us_state_id => us_state_id, :name => (at_large? ? '1' : district.to_s)})
   end
   delegate :the_geom, :envelope, :polygon, :linear_ring, :display_name, :level, :to => :district_geometry
 
