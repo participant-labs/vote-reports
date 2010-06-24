b96783785b1d9fefdacde36989c80186a30df863
Ben Woosley <ben.woosley@gmail.com>
2010-06-24 02:41:36 -0400

    Adapt the Guide to a post-zip world

--- app/controllers/guides_controller.rb
+++ app/controllers/guides_controller.rb
@@ -17,24 +17,6 @@ class GuidesController < ApplicationController
         render :action => next_step, :layout => false
       }
     end
-
-    # @dont_show_geo_address = true
-    # 
-    # if params[:representing].present? && zip_code?(params[:representing])
-    #   session[:zip_code] = params[:representing]
-    # end
-    # 
-    # params[:in_office] = true if params[:in_office].nil?
-    # @politicians = sought_politicians.all(:limit => 5)
-    # 
-    # @causes = Cause.all(:order => 'name')
-    #
-    # respond_to do |format|
-    #   format.html
-    #   format.js {
-    #     render :layout => false
-    #   }
-    # end
   end
 
   def create
