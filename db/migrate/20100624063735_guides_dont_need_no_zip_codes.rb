23745b3ae02e3ede2df5bff1e61651a981e92c9a
Ben Woosley <ben.woosley@gmail.com>
2010-06-30 19:12:16 -0400

    Revamp migrations to rename districts to congressional districts earlier, so we can migrate from the earlier days...

--- db/migrate/20100624063735_guides_dont_need_no_zip_codes.rb
+++ db/migrate/20100624063735_guides_dont_need_no_zip_codes.rb
@@ -1,12 +1,10 @@
 class GuidesDontNeedNoZipCodes < ActiveRecord::Migration
   def self.up
     remove_column :guides, :zip_code, :plus_4
-    rename_column :guides, :district_id, :congression_district_id
   end
 
   def self.down
     add_column :guides, :zip_code_id, :integer
     add_column :guides, :plus_4, :integer
-    rename_column :guides, :congression_district_id, :district_id
   end
 end
