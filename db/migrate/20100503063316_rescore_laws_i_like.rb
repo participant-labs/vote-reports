4adb72620021436f41aa8a81e389d159b02c2ec5
Ben Woosley <ben.woosley@gmail.com>
2010-06-30 22:06:47 -0400

    Only rescore the laws_i_like reports in the rescore task

--- db/migrate/20100503063316_rescore_laws_i_like.rb
+++ db/migrate/20100503063316_rescore_laws_i_like.rb
@@ -1,7 +1,7 @@
 class RescoreLawsILike < ActiveRecord::Migration
   def self.up
     # rescore all as ReportSubjects at least are out of sync
-    Report.all.map(&:rescore!)
+    Report.laws_i_like.each(&:rescore!)
   end
 
   def self.down
