97d67a2e49f63713582572598f943da088891e84
Ben Woosley <ben.woosley@gmail.com>
2010-07-10 12:36:10 -0400

    Inline hover_image_attrs so we don't knock out our classes

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,20 +1,12 @@
 module ImagesHelper
-  def hover_image_attrs(image)
-    attachment = image.thumbnail
-    if attachment.file?
-      {
-        :class => "act-qtip-image qtip-width-#{attachment.styles[:large][:geometry].split('x').first.to_i}",
-        :rel => attachment.url(:large)
-      }
-    else
-      {}
-    end
-  end
-
   def image_with_hover_tag(image, title, attrs = {})
     style = attrs.delete(:style) || image.thumbnail.default_style
     attrs.reverse_merge!(:alt => title, :title => title)
-    attrs.merge!(hover_image_attrs(image)) if style != :large
+    if style != :large && image.thumbnail.file?
+      attrs[:rel] = image.thumbnail.url(:large)
+      attrs[:class] ||= ''
+      attrs[:class] += " act-qtip-image qtip-width-#{image.thumbnail.styles[:large][:geometry].split('x').first.to_i}"
+    end
     image_tag(image.thumbnail.url(style), attrs)
   end
 end
