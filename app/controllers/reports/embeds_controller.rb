fdbc0d80487030e42da2bc0693ca889b3a2a5b2c
Ben Woosley <ben.woosley@gmail.com>
2010-07-16 12:41:39 -0400

    Add pagination to report embed

--- app/controllers/reports/embeds_controller.rb
+++ app/controllers/reports/embeds_controller.rb
@@ -3,7 +3,15 @@ class Reports::EmbedsController < ApplicationController
   layout 'minimal'
 
   def show
-    @scores = @report.scores.for_politicians(sought_politicians)
+    @scores = @report.scores.for_politicians(sought_politicians).paginate :page => params[:page], :per_page => 3
+
+    respond_to do |format|
+      format.html
+      format.js {
+        render :partial => 'reports/embeds/scores',
+          :locals => {:scores => @scores, :replace => 'embeded_report_scores'}
+      }
+    end
   end
 
   private
