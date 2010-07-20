e06b4fe5ecc92eb9c05f4a82543c6e75f6200b48
Ben Woosley <ben.woosley@gmail.com>
2010-07-20 11:11:25 -0400

    Switch it up such that logged-in users get their dashboard on the root path

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,30 +1,40 @@
 class SiteController < ApplicationController
 
   def index
-    params[:in_office] = true
+    if current_user
+      dashboard
+    else
+      params[:in_office] = true
 
-    @politicians =
-      if shown_location
-        sought_politicians
-      else
-        Politician.in_office
-      end.scoped(:limit => 6)
-    @sample_report = Report.published.with_scores_for(@politicians).random.first
-    @scores = @sample_report.scores.for_politicians(@politicians)
+      @politicians =
+        if shown_location
+          sought_politicians
+        else
+          Politician.in_office
+        end.scoped(:limit => 6)
+      @sample_report = Report.published.with_scores_for(@politicians).random.first
+      @scores = @sample_report.scores.for_politicians(@politicians)
 
-    respond_to do |format|
-      format.html {
-        @recent_reports = Report.user_published.by_created_at.all(:limit => 3)
-      }
-      format.js {
-        render :partial => 'reports/report_and_scores', :locals => {
-          :report => @sample_report,
-          :scores => @scores
+      respond_to do |format|
+        format.html {
+          @recent_reports = Report.user_published.by_created_at.all(:limit => 3)
         }
-      }
+        format.js {
+          render :partial => 'reports/report_and_scores', :locals => {
+            :report => @sample_report,
+            :scores => @scores
+          }
+        }
+      end
     end
   end
 
   def about
+  end
+
+  def dashboard
+    @user = current_user
+    @reports = current_user.reports
+    render :template => 'users/show'
   end
 end
