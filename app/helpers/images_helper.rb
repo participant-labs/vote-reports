0a579678da99abe288da8685e1abd8e92040cabd
Ben Woosley <ben.woosley@gmail.com>
2010-07-23 14:31:48 -0400

    Use the paperclip dimensions in our html

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -2,6 +2,7 @@ module ImagesHelper
   def image_with_hover_tag(image, title, attrs = {})
     style = attrs.delete(:style) || image.thumbnail.default_style
     attrs.reverse_merge!(:alt => title, :title => title)
+    attrs.reverse_merge!(:size => image.send(:"thumbnail_#{style}_size")) if image.respond_to?(:"thumbnail_#{style}_size")
     if style != :large && image.thumbnail.file?
       attrs[:rel] = image.thumbnail.url(:large)
       attrs[:class] ||= ''
