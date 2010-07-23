75304793332e18e654c631d4cdb980bc610fdfa9
Ben Woosley <ben.woosley@gmail.com>
2010-07-23 17:24:36 -0400

    Drop qtip image hovers. They're distracting, a bit buggy and the payoff is no good

--- app/helpers/images_helper.rb
+++ app/helpers/images_helper.rb
@@ -1,13 +1,8 @@
 module ImagesHelper
-  def image_with_hover_tag(image, title, attrs = {})
+  def sized_image_tag(image, title, attrs = {})
     style = attrs.delete(:style) || image.thumbnail.default_style
     attrs.reverse_merge!(:alt => title, :title => title)
     attrs.reverse_merge!(:size => image.send(:"thumbnail_#{style}_size")) if image.respond_to?(:"thumbnail_#{style}_size")
-    if style != :large && image.thumbnail.file?
-      attrs[:rel] = image.thumbnail.url(:large)
-      attrs[:class] ||= ''
-      attrs[:class] += " act-qtip-image qtip-width-#{image.thumbnail.styles[:large][:geometry].split('x').first.to_i}"
-    end
     image_tag(image.thumbnail.url(style), attrs)
   end
 end
