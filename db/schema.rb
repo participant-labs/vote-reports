1caf7919fdd08faeb8ded8ffcb2c9f1ec18deb0d
Ben Woosley <ben.woosley@gmail.com>
2010-07-24 15:29:19 -0400

    A migration to correct more interest group ratings falsely attributed to Bobby Jindal

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100723183705) do
+ActiveRecord::Schema.define(:version => 20100724192316) do
 
   create_table "adminships", :force => true do |t|
     t.integer  "user_id",       :null => false
