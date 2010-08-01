ae00a7d42f869d7c495a575f8b690bd5d675dffb
Ben Woosley <ben.woosley@gmail.com>
2010-08-01 01:51:15 -0400

    We need to work off more jobs in test to properly set up our reports

--- spec/support/blueprints.rb
+++ spec/support/blueprints.rb
@@ -360,7 +360,7 @@ Fixjour :verify => false do
   def create_unscored_report(attrs = {})
     create_report(attrs).tap do |report|
       create_bill_criterion(:report => report)
-      Delayed::Worker.new(:quiet => true).work_off(1)
+      Delayed::Worker.new(:quiet => true).work_off(5)
     end
   end
 
@@ -372,7 +372,7 @@ Fixjour :verify => false do
         create_vote(:roll => roll, :politician => p, :vote => Vote::POSSIBLE_VALUES.random_element)
       end
       report.rescore!
-      Delayed::Worker.new(:quiet => true).work_off(1)
+      Delayed::Worker.new(:quiet => true).work_off(5)
     end
   end
 
