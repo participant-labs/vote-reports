7d1d43cccef4c832b9105ac47e626f02bc2ce175
Ben Woosley <ben.woosley@gmail.com>
2010-08-01 16:58:20 -0400

    Let's get crazy (in dealing with 'closed stream' exception - connect to mongo at the top of the Scorer#perform method

--- app/models/report/scorer.rb
+++ app/models/report/scorer.rb
@@ -8,6 +8,15 @@ class Report
     end
 
     def perform
+      require 'mongo_mapper'
+
+      config = YAML.load_file(Rails.root.join('config', 'mongo.yml'))
+      MongoMapper.setup(config, Rails.env, {
+        :logger    => Rails.logger,
+        :passenger => true,
+      })
+      MongoMapper.database = config[Rails.env]['database']
+
       rescue_and_reraise do
         require 'ar-extensions'
         require 'ar-extensions/import/postgresql'
