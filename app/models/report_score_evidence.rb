6cf93e9526b81ffe36ce1d0adbd478f7bc741237
Ben Woosley <ben.woosley@gmail.com>
2010-08-01 00:34:14 -0400

    Slightly more serviceable evidence show page for scores with multiple evidence types.

--- app/models/report_score_evidence.rb
+++ app/models/report_score_evidence.rb
@@ -4,6 +4,7 @@ class ReportScoreEvidence < ActiveRecord::Base
   belongs_to :score, :class_name => 'ReportScore'
 
   named_scope :interest_group_ratings, :conditions => {:criterion_type => 'InterestGroupReport'}
+  named_scope :not_interest_group_ratings, :conditions => ["report_score_evidences.criterion_type !=  ?", 'InterestGroupReport']
   named_scope :votes, :conditions => {:criterion_type => 'BillCriterion'}
 
   # validates_presence_of :evidence, :score, :criterion
