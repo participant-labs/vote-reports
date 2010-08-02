97c5dc56d57a18f0ee88894d31cf5b99f7ad70fa
Ben Woosley <ben.woosley@gmail.com>
2010-08-02 00:10:11 -0400

    Consolidate interest group and user report subjects under a single common controller & views.  Also, add InterestGroup#vote_smart_url and use it.

--- app/controllers/interest_groups/subjects_controller.rb
+++
@@ -1,8 +0,0 @@
-class InterestGroups::SubjectsController < ApplicationController
-  def index
-    @interest_group = InterestGroup.find(params[:interest_group_id], :include => {:report => :subjects})
-    @report = @interest_group.report
-    @subjects = @report.subjects
-    render :layout => false
-  end
-end
