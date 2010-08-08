e1f558ea3ac9d232d1981208d202c31c3b8d502d
Ben Woosley <ben.woosley@gmail.com>
2010-08-08 15:37:54 -0400

    Cache interest group agendas

---
+++ app/sweepers/interest_group_sweeper.rb
@@ -0,0 +1,21 @@
+class InterestGroupSweeper < ActionController::Caching::Sweeper
+  observe InterestGroup
+
+  def after_create(interest_group)
+    expire_cache_for(interest_group)
+  end
+
+  def after_update(interest_group)
+    expire_cache_for(interest_group)
+  end
+
+  def after_destroy(interest_group)
+    expire_cache_for(interest_group)
+  end
+
+  private
+
+  def expire_cache_for(interest_group)
+    expire_page interest_group_agenda_path(interest_group)
+  end
+end
