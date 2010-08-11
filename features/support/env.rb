b68b3c5b49bdeb09c9f0181d78d8e45c11d51d5b
Ben Woosley <ben.woosley@gmail.com>
2010-08-10 23:09:58 -0400

    Enable database_cleaner in cucumber (so we don't get build-up in our mongo)

--- features/support/env.rb
+++ features/support/env.rb
@@ -49,10 +49,5 @@ ActionController::Base.allow_rescue = false
 Cucumber::Rails::World.use_transactional_fixtures = true
 # How to clean your database when transactions are turned off. See
 # http://github.com/bmabey/database_cleaner for more info.
-if defined?(ActiveRecord::Base)
-  begin
-    require 'database_cleaner'
-    DatabaseCleaner.strategy = :truncation
-  rescue LoadError => ignore_if_database_cleaner_not_present
-  end
-end
+require 'database_cleaner'
+DatabaseCleaner.strategy = :truncation
