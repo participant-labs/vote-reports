cadb9e5553e0488d3deecebda5da5fcd79342641
Ben Woosley <ben.woosley@gmail.com>
2010-08-17 01:54:31 -0400

    Cache cause reports index

---
+++ app/models/cause_report_sweeper.rb
@@ -0,0 +1,21 @@
+class CauseReportSweeper < ActionController::Caching::Sweeper
+  observe CauseReport
+
+  def after_create(cause_report)
+    expire_cache_for(cause_report)
+  end
+
+  def after_update(cause_report)
+    expire_cache_for(cause_report)
+  end
+
+  def after_destroy(cause_report)
+    expire_cache_for(cause_report)
+  end
+
+  private
+
+  def expire_cache_for(cause_report)
+    expire_page cause_reports_path(cause_report.cause)
+  end
+end
