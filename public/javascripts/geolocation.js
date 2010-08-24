4f4faeb66dac06751ee8367f7e5474c51a8d0331
Ben Woosley <ben.woosley@gmail.com>
2010-08-24 17:43:00 -0400

    Drop automatic location setter as the dialog box is annoying - we need to make this intentional - as a result of a button click or something

--- public/javascripts/geolocation.js
+++ public/javascripts/geolocation.js
@@ -7,31 +7,3 @@ function togglePolygons(map, polygons) {
     }
   });
 }
-
-;(function($) {
-  $(function() {
-    if(location_set) {
-    } else if(navigator.geolocation) {
-      // Try W3C Geolocation (Preferred)
-      navigator.geolocation.getCurrentPosition(function(position) {
-        $.post('/location', {
-          autoloc: {
-            lat: position.coords.latitude,
-            lng: position.coords.longitude
-          }
-        });
-      });
-    } else if (google.gears) {
-      // Try Google Gears Geolocation
-      var geo = google.gears.factory.create('beta.geolocation');
-      geo.getCurrentPosition(function(position) {
-        $.post('/location', {
-          autoloc: {
-            lat: position.latitude,
-            lng: position.longitude
-          }
-        });
-      });
-    }
-  });
-})(jQuery);
