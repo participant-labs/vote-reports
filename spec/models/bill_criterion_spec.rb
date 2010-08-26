31bcde4534ab3238fed98d1b7392e0fa7bfbcc33
Ben Woosley <ben.woosley@gmail.com>
2010-08-26 02:20:26 -0400

    Add a spec of the adc html

--- spec/models/bill_criterion_spec.rb
+++ spec/models/bill_criterion_spec.rb
@@ -56,26 +56,37 @@ describe BillCriterion do
   end
 
   describe ".autofetch!" do
-    it "should extract supported/opposed bill names and positions from an html source" do
-      BillCriterion.autofetch_from(Rails.root.join('spec/fixtures/files/capwiz/consumer_action.html')).should include({
-        "S. 1799 - FAIR Overdraft Coverage Act" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=14361426'},
-        "S. 202 - Passenger Vehicle Loss Disclosure Act" => {:support => false, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=13009046'},
-        "H.R. 5381 - Motor Vehicle Safety Act of 2010" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=16046886'},
-        "S. 399 - Credit Card Holder's Bill of Rights" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=12896581'},
-        "S. 392 - Credit card consumer protections" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=12896561'},
-        "S. 165 - Student credit card protections" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=12896391'},
-        "S. 131 - Credit card mimimum payment notice" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=12896616'}
-      })
+    context "from a customized html source" do
+      it "should extract supported/opposed bill names and positions" do
+        BillCriterion.autofetch_from(Rails.root.join('spec/fixtures/files/capwiz/consumer_action.html')).should include({
+          "S. 1799 - FAIR Overdraft Coverage Act" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=14361426'},
+          "S. 202 - Passenger Vehicle Loss Disclosure Act" => {:support => false, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=13009046'},
+          "H.R. 5381 - Motor Vehicle Safety Act of 2010" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=16046886'},
+          "S. 399 - Credit Card Holder's Bill of Rights" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=12896581'},
+          "S. 392 - Credit card consumer protections" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=12896561'},
+          "S. 165 - Student credit card protections" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=12896391'},
+          "S. 131 - Credit card mimimum payment notice" => {:support => true, :explanatory_url => 'http://capwiz.com/consumeraction/issues/bills/?bill=12896616'}
+        })
+      end
+
+      it "should not include those with dashes or monitoring" do
+        BillCriterion.autofetch_from(Rails.root.join('spec/fixtures/files/capwiz/consumer_action.html')).keys.should_not include(
+          "H.R. 1880 - Insurance consumer protection",
+          "H.R. 111 - Prevent banks from real estate brokerage/management",
+          "S. 486 - Access for all to comprehensive primary health care services",
+          "S. 436 - Youth safety on the Internet",
+          "S. 348 - Universal service contributions"
+        )
+      end
     end
 
-    it "should not include those with dashes or monitoring" do
-      BillCriterion.autofetch_from(Rails.root.join('spec/fixtures/files/capwiz/consumer_action.html')).keys.should_not include(
-        "H.R. 1880 - Insurance consumer protection",
-        "H.R. 111 - Prevent banks from real estate brokerage/management",
-        "S. 486 - Access for all to comprehensive primary health care services",
-        "S. 436 - Youth safety on the Internet",
-        "S. 348 - Universal service contributions"
-      )
+    context "from a standard source" do
+      it "should extract supported/opposed bill names and positions" do
+        BillCriterion.autofetch_from(Rails.root.join('spec/fixtures/files/capwiz/adc.html')).should include({
+          "S. 416 - 'A bill to limit the use of cluster munitions. '" => {:support => true, :explanatory_url => 'http://capwiz.com/adc/issues/bills/?bill=12956486'},
+          "H.R. 1206 - 'To strengthen sanctions against the Government of Syria, to enhance multilateral commitment to address the Government of Syria''s threatening policies, to establish a program to support a transition to a democratically-elected government in Syria, and for other purposes. '" => {:support => false, :explanatory_url => 'http://capwiz.com/adc/issues/bills/?bill=12943491'}
+        })
+      end
     end
   end
 end
