67cf3f7b5aea82d5f27d2e16e0ba31d19f330b01
Ben Woosley <ben.woosley@gmail.com>
2010-08-29 15:07:31 -0400

    Revert "Switch to js for embed widget"
    This reverts commit 69097e18f254af1fcc9b371df405f15c1b8644dd.
    
    Conflicts:
    
    	app/controllers/reports/embeds_controller.rb
    	app/helpers/reports_helper.rb
    	app/views/reports/embeds/show.html.erb

--- app/controllers/embeds/reports_controller.rb
+++ app/controllers/embeds/reports_controller.rb
@@ -4,16 +4,6 @@ class Embeds::ReportsController < ApplicationController
 
   def show
     @scores = @report.scores.for_politicians(sought_politicians).paginate :page => params[:page], :per_page => 3
-
-    if params[:page]
-      render :partial => 'embeds/reports/scores',
-        :locals => {:scores => @scores, :replace => 'embeded_report_scores'}
-    else
-      render :js => %{
-        document.getElementById("#{report_embed_id(@report)}").innerHTML =
-          #{js_render(:action => 'show', :css => 'reports')};
-      }
-    end
   end
 
   private
--- app/controllers/reports/embeds_controller.rb
+++ app/controllers/reports/embeds_controller.rb
@@ -1,7 +1,14 @@
 class Reports::EmbedsController < ApplicationController
+  before_filter :load_report
   layout nil
 
   def show
+    @scores = @report.scores.for_politicians(sought_politicians).paginate :page => params[:page], :per_page => 3
+  end
+
+  private
+
+  def load_report
     @report = User.find(params[:user_id]).reports.find(params[:report_id])
   end
 end
