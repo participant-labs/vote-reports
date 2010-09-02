3ef1ef9e40fc7ebc472b2961d32e8cb4e4649f63
Ben Woosley <ben.woosley@gmail.com>
2010-09-02 03:27:20 -0400

    Correct ellipsization to not truncate those who fit in the limit

--- lib/extensions/core_extensions.rb
+++ lib/extensions/core_extensions.rb
@@ -1,6 +1,7 @@
 class String
   def ellipsize(len = 9)
+    return self if size <= len || size <= 3
     len -= 3
-    gsub(%r{(.{#{len / 2}}).*(.{#{len / 2}})}, '\1...\2')
+    gsub(%r{(.{#{(len + 1) / 2}}).*(.{#{len / 2}})}, '\1...\2')
   end
 end
