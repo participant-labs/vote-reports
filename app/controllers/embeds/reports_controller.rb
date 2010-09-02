f9fb84f6a14fad50652b40258cc8e7ddd2c98f2d
Ben Woosley <ben.woosley@gmail.com>
2010-09-02 00:51:32 -0400

    When serving a random report, ensure it has at least one score in the service area

--- app/controllers/embeds/reports_controller.rb
+++ app/controllers/embeds/reports_controller.rb
@@ -1,21 +1,17 @@
 class Embeds::ReportsController < ApplicationController
-  before_filter :load_report
   layout 'widget'
 
   def show
     @location = current_geo_location || Location.random.first
     @politicians = Politician.from_location(@location).in_office_normal_form.all(:limit => 8)
-    @scores = @report.scores.for_politicians(@politicians).all
-  end
 
-  private
-
-  def load_report
     @report =
       if params[:id] == 'random'
-        Report.published.random.first
+        Report.published.with_scores_for(@politicians).random.first
       else
         Report.published.find(Integer(params[:id]))
       end
+
+    @scores = @report.scores.for_politicians(@politicians).all
   end
 end
