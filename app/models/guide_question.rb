4471a3360478fcffd416e5267ae0e6c5ae1bba7a
Ben Woosley <ben.woosley@gmail.com>
2010-09-05 15:03:50 -0400

    Include the issue causes, as we use them immediately

--- app/models/guide_question.rb
+++ app/models/guide_question.rb
@@ -1,6 +1,6 @@
 class GuideQuestion
   def self.questions
-    (Issue.all.map {|issue|
+    (Issue.all(:include => :causes).map {|issue|
       {:question => "What's your view on #{issue.title.downcase}?", :object => issue, :options => issue.causes.inject({}) {|accum, cause| accum[cause] = cause.name; accum }}
     } + Cause.without_issue.map {|cause| 
       {:question => "What's your view on #{cause.name.downcase}?", :object => cause, :options => {cause => 'Support'}}
