6937744cfb17d0cb6fdd9b559c3eaff3ee31d87c
Ben Woosley <ben.woosley@gmail.com>
2010-09-06 00:04:43 -0400

    Use tipsy for the guide cause buttons

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -16,6 +16,8 @@
     $('.flash.success, .flash.notice, .flash.message').fadeOutSoon(3000);
     $('.flash.error, .flash.warning').fadeOutSoon(5000, 5000);
 
+    $('.act-tipsy').tipsy({fade: true, gravity: 'n'});
+
     $('.selectable').live('update_selected', function(event) {
       $(event.target).closest('.selectable').toggleClass('selected');
       return true;
