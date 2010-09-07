cbbbab17bcf5c53c261bfa3f84d7cbd7daf2323c
Ben Woosley <ben.woosley@gmail.com>
2010-09-07 03:41:25 -0400

    Unbreak widgets

--- app/controllers/embeds/reports_controller.rb
+++ app/controllers/embeds/reports_controller.rb
@@ -5,14 +5,13 @@ class Embeds::ReportsController < ApplicationController
     @location = current_geo_location || Location.random.first
     @politicians = Politician.from_location(@location).in_office_normal_form.by_prominance.all(:limit => 8, :select => "DISTINCT politicians.*, #{Politician.prominence_clause}")
 
-    @report =
-      if params[:id] == 'random'
-        target_id = report_embed_id('random')
-        Report.published.with_scores_for(@politicians).random.first || Report.published.random.first
-      else
-        target_id = report_embed_id(@report)
-        Report.published.find(Integer(params[:id]))
-      end
+    if params[:id] == 'random'
+      @report = Report.published.with_scores_for(@politicians).random.first || Report.published.random.first
+      target_id = report_embed_id('random')
+    else
+      @report = Report.published.find(Integer(params[:id]))
+      target_id = report_embed_id(@report)
+    end
 
     @scores = @report.scores.for_politicians(@politicians).all
 
