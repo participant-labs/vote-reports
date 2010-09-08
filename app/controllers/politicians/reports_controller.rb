79d73973c902e62e7b9c322b0ced4ef8e78529c6
Ben Woosley <ben.woosley@gmail.com>
2010-09-07 23:44:34 -0400

    Clean up politicians /eports db access with include scopes

--- app/controllers/politicians/reports_controller.rb
+++ app/controllers/politicians/reports_controller.rb
@@ -5,9 +5,9 @@ class Politicians::ReportsController < ApplicationController
     params[:subjects] ||= []
     if params[:term].present?
       @reports = Report.paginated_search(params).results
-      @scores = @reports.replace(topical_scores.for_reports(@reports).all(:include => :report))
+      @scores = @reports.replace(topical_scores.for_reports(@reports).all)
     else
-      @scores = topical_scores.paginate(:page => params[:page], :include => :report)
+      @scores = topical_scores.paginate(:page => params[:page])
     end
 
     @subjects = Subject.for_report(@scores.map(&:report)).for_tag_cloud.all(:limit => 20)
@@ -25,6 +25,6 @@ class Politicians::ReportsController < ApplicationController
   private
 
   def topical_scores
-    @politician.report_scores.for_published_reports.for_reports_with_subjects(params[:subjects])
+    @politician.report_scores.for_published_reports.for_politician_display.for_reports_with_subjects(params[:subjects])
   end
 end
