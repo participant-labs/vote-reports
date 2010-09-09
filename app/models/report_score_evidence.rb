c4782ca983ac41addac2e207924390d6b1d64a30
Ben Woosley <ben.woosley@gmail.com>
2010-09-09 00:42:16 -0400

    Fix that ReportScoreEvidence#score was looking for the score_id column when we have report_score_id in the table.

--- app/models/report_score_evidence.rb
+++ app/models/report_score_evidence.rb
@@ -5,7 +5,7 @@ class ReportScoreEvidence < ActiveRecord::Base
 
   belongs_to :evidence, :polymorphic => true
   belongs_to :criterion, :polymorphic => true
-  belongs_to :score, :class_name => 'ReportScore'
+  belongs_to :score, :class_name => 'ReportScore', :foreign_key => 'report_score_id'
 
   named_scope :interest_group_ratings, :conditions => {:criterion_type => 'InterestGroupReport'}
   named_scope :not_interest_group_ratings, :conditions => ["report_score_evidences.criterion_type !=  ?", 'InterestGroupReport']
