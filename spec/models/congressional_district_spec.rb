7dcb0f8dfd1f7d5eaccee64561123b5fada3c75d
Ben Woosley <ben.woosley@gmail.com>
2010-09-09 00:26:28 -0400

    Add a test to show that CongressionalDistrict#for_city excludes congressional districts without that state even if they're attached to a stately zip code

--- spec/models/congressional_district_spec.rb
+++ spec/models/congressional_district_spec.rb
@@ -11,14 +11,24 @@ describe CongressionalDistrict do
       create_location(:city => 'SAN FRANCISCO', :state => 'NM', :zip_code => '11111')
     end
 
-    it "should handle cities alone" do
-      CongressionalDistrict.for_city('san francisco').should =~ [@nm_district, @ca_district]
-      CongressionalDistrict.for_city('San francisco').should =~ [@nm_district, @ca_district]
+    context "when a city is supplied" do
+      it "should return districts for any such city, regardless of state" do
+        CongressionalDistrict.for_city('san francisco').should =~ [@nm_district, @ca_district]
+        CongressionalDistrict.for_city('San francisco').should =~ [@nm_district, @ca_district]
+      end
     end
 
-    it "should handle regular cities" do
-      CongressionalDistrict.for_city('san francisco, ca').should == [@ca_district]
-      CongressionalDistrict.for_city('san FRANCISCO, ca').should == [@ca_district]
+    context "when a city & state is supplied" do
+      it "should handle regular cities" do
+        CongressionalDistrict.for_city('san francisco, ca').should == [@ca_district]
+        CongressionalDistrict.for_city('san FRANCISCO, ca').should == [@ca_district]
+      end
+
+      it "should not return results from bordering states even when they have the same zip code" do
+        borders_ca_district = create_congressional_district(:state => create_us_state(:abbreviation => 'AZ'))
+        borders_ca_district.zip_codes << @ca_district.zip_codes.first
+        CongressionalDistrict.for_city('san francisco, ca').should_not include(borders_ca_district)
+      end
     end
   end
 end
