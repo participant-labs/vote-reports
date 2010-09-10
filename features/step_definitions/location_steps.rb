19be137bea5545d5f59211c93eb1978f2139e85e
Ben Woosley <ben.woosley@gmail.com>
2010-09-10 00:12:03 -0400

    Supply lat & lng in scenario geoloc setting to avoid errors on district lookup

--- features/step_definitions/location_steps.rb
+++ features/step_definitions/location_steps.rb
@@ -1,6 +1,6 @@
 Transform /location is "([^\"]*)"/ do |address|
   city, state, zip = address.match(/(.+), ([A-Z]{2}) (\d{5})/).captures
-  Geokit::GeoLoc.new(:city => city, :state => state, :zip => zip, :country_code => 'US').tap do |loc|
+  Geokit::GeoLoc.new(:city => city, :state => state, :zip => zip, :country_code => 'US', :lat => '33.0146', :lng => '-97.0970').tap do |loc|
     loc.success = true
   end
 end
