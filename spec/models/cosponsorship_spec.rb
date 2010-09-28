bbc99d51a3fe15f0b27a02b290ce48bae49e4780
Ben Woosley <ben.woosley@gmail.com>
2010-09-28 01:25:35 -0400

    Drop scaffolded test

--- spec/models/cosponsorship_spec.rb
+++ spec/models/cosponsorship_spec.rb
@@ -1,15 +1,4 @@
 require 'spec_helper'
 
 describe Cosponsorship do
-  before(:each) do
-    @valid_attributes = {
-      :bill_id => 1,
-      :politician_id => 1,
-      :joined_on => Date.today
-    }
-  end
-
-  it "should create a new instance given valid attributes" do
-    Cosponsorship.create!(@valid_attributes)
-  end
 end
