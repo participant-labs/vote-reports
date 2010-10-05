3efb7c33236e9d97dcefdf08bd1163bcfd550776
Ben Woosley <ben.woosley@gmail.com>
2010-10-05 00:25:10 -0700

    Show the politician headshot alongside the politician on the guide, and fall back to the vote_smart_photo_url if gov_track photo isn't present

--- app/models/politician/gov_track.rb
+++ app/models/politician/gov_track.rb
@@ -2,11 +2,10 @@ class Politician < ActiveRecord::Base
   module GovTrack
     def headshot
       @headshot ||= begin
-        if gov_track_id.blank?
-          notify_hoptoad("Unable to fetch headshot, no gov_track_id for: #{inspect}")
+        if gov_track_id.blank? && vote_smart_photo_url.blank?
           return nil
         end
-        GovTrack::Headshot.new(gov_track_id)
+        GovTrack::Headshot.new(gov_track_id, vote_smart_photo_url)
       end
     end
 
@@ -16,9 +15,10 @@ class Politician < ActiveRecord::Base
       TYPE_TO_WIDTH = {nil => nil, :tiny => '50px', :small => '50px', :medium => '100px', :large => '200px'}.freeze
       TYPE_TO_SIZE = {:tiny => '25x30', :small => '50x60', :medium => '100x120', :large => '200x240'}.freeze
 
-      def initialize(id)
-        raise ArgumentError unless id.present?
-        @id = id
+      def initialize(gov_track_id, vote_smart_photo_url)
+        raise ArgumentError unless gov_track_id.present? || vote_smart_photo_url.present?
+        @gov_track_id = gov_track_id
+        @vote_smart_photo_url = vote_smart_photo_url
       end
 
       def thumbnail
@@ -40,8 +40,12 @@ class Politician < ActiveRecord::Base
       end
 
       def url(size = nil)
-        size = TYPE_TO_WIDTH.fetch(size)
-        URI.join(ROOT_PATH, 'photos/', "#{[@id, size].compact.join('-')}.jpeg").to_s
+        if @id
+          size = TYPE_TO_WIDTH.fetch(size)
+          URI.join(ROOT_PATH, 'photos/', "#{[@gov_track_id, size].compact.join('-')}.jpeg").to_s
+        else
+          @vote_smart_photo_url if @vote_smart_photo_url.try(:ends_with?, '.jpg')
+        end
       end
     end
   end
