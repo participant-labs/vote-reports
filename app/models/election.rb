e557db26336e097e3c902c7c367c5d86953d4612
Ben Woosley <ben.woosley@gmail.com>
2010-10-04 17:24:28 -0700

    Select out the proper races from the DB for the guide, but just drop them into the screen, rather than formatting them nicely

--- app/models/election.rb
+++ app/models/election.rb
@@ -1,5 +1,7 @@
 class Election < ActiveRecord::Base
   belongs_to :state, :class_name => 'UsState'
+  belongs_to :office_type
+
   has_many :stages, :class_name => 'ElectionStage'
   has_many :races, :through => :stages
 end
--- app/models/election_stage.rb
+++ app/models/election_stage.rb
@@ -1,4 +1,7 @@
 class ElectionStage < ActiveRecord::Base
   belongs_to :election
   has_many :races
+
+  named_scope :future, :conditions => ['voted_on > ?', Date.today]
+  named_scope :by_voted_on, :order => 'voted_on desc'
 end
