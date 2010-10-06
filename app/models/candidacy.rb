dce129656c3120312185766b7cc1929ffdafae2b
Ben Woosley <ben.woosley@gmail.com>
2010-10-05 22:45:04 -0700

    Show candidate scores in the guide table

--- app/models/candidacy.rb
+++ app/models/candidacy.rb
@@ -3,4 +3,13 @@ class Candidacy < ActiveRecord::Base
   belongs_to :race
 
   named_scope :valid, :conditions => ['status NOT IN(?)', ["Deceased", "Withdrawn", "Removed"]]
+  named_scope :for_races, lambda {|races|
+    {:conditions => {:race_id => races}}
+  }
+
+  class << self
+    def for_districts(districts)
+      for_races(Race.for_districts(districts))
+    end
+  end
 end
