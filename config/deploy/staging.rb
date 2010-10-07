957a8096894f71fbd2ce0b427cdf64d6d8709eff
Ben Woosley <ben.woosley@gmail.com>
2010-10-07 15:45:56 -0700

    Actually, drop the staging solr altogether as it was only used for the ci

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -2,11 +2,3 @@ set :domain, 'staging.votereports.org'
 set :rails_env, "staging"
 
 server domain, :app, :web, :db, :primary => true
-
-namespace :sunspot do
-  desc "Update the crontab file"
-  task :start, :roles => :app do
-    run "cd #{current_path} && rake sunspot:solr:start RAILS_ENV=production"
-  end
-  after "deploy:symlink", "sunspot:start"
-end
