246d8625145e52338ee135cd7d83283c15a54d29
Ben Woosley <ben.woosley@gmail.com>
2010-10-06 18:41:40 -0700

    Include the report in the scores api json, so we don't have to repeat it in the scores

--- app/controllers/interest_groups/scores_controller.rb
+++ app/controllers/interest_groups/scores_controller.rb
@@ -12,7 +12,7 @@ class InterestGroups::ScoresController < ApplicationController
         }
       }
       format.json {
-        render :json => @report.scores
+        render :json => @report.as_json.merge(:scores => @report.scores.as_json)
       }
     end
   end
