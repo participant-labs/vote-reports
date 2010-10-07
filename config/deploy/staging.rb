0eaa78142b05fcc6b948960cfd861d0f31ebae47
Ben Woosley <ben.woosley@gmail.com>
2010-10-07 15:42:34 -0700

    Try just starting solr for production on deploys

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -6,7 +6,7 @@ server domain, :app, :web, :db, :primary => true
 namespace :sunspot do
   desc "Update the crontab file"
   task :start, :roles => :app do
-    run "cd #{current_path} && rake sunspot:start"
+    run "cd #{current_path} && rake sunspot:solr:start RAILS_ENV=production"
   end
   after "deploy:symlink", "sunspot:start"
 end
