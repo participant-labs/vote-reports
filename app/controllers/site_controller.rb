fca9a36fce95f6e1c3c8bda22c99d8bfcddeb492
Ben Woosley <ben.woosley@gmail.com>
2010-10-11 18:11:43 -0700

    OMG I was still doing a bunch of unnecessary db access in the front page (from the prior design)

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -4,23 +4,7 @@ class SiteController < ApplicationController
     if current_user
       dashboard
     else
-      params[:in_office] = true
-
-      @politicians =
-        if shown_location
-          sought_politicians
-        else
-          Politician.in_office
-        end.scoped(:limit => 6)
-      @sample_report = Report.with_scores_for(@politicians).published.random.for_display.first
-      @scores = @sample_report.scores.for_politicians(@politicians).for_report_display
-
-      respond_to do |format|
-        format.html {
-          @recent_reports = Report.user_published.by_created_at.for_display.all(:limit => 3)
-          render :layout => 'footer_only'
-        }
-      end
+      render :layout => 'footer_only'
     end
   end
 
