c9c30b26e4da213c07bbf287df0642e9a200cf41
Ben Woosley <ben.woosley@gmail.com>
2010-10-11 23:24:16 -0700

    Capitalize "# Congressional District" wherever it appears

--- app/helpers/us/congressional_districts_helper.rb
+++ app/helpers/us/congressional_districts_helper.rb
@@ -1,15 +1,8 @@
 module Us::CongressionalDistrictsHelper
   def district_full_name(district)
-    [link_to_unless_current("the #{district.which.downcase} congressional district", congressional_district_path(district)),
-      state_name(district.state)
-    ].join(' of ')
-  end
-  safe_helper :district_full_name
-
-  def district_full_title_name(district)
     [link_to_unless_current("#{district.which} Congressional District", congressional_district_path(district)),
      state_name(district.state)
     ].join(' of ')
   end
-  safe_helper :district_full_title_name
+  safe_helper :district_full_name
 end
