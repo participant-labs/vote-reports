e71baaecfea9200b08e899bc8b0ef7f5bb8354e5
Ben Woosley <ben.woosley@gmail.com>
2010-10-12 12:24:11 -0700

    Drop unused js responses for state & congressional districts

--- app/controllers/us/congressional_districts_controller.rb
+++ app/controllers/us/congressional_districts_controller.rb
@@ -2,16 +2,8 @@ class Us::CongressionalDistrictsController < ApplicationController
   def show
     @district = CongressionalDistrict.find_by_name(params[:id])
 
-    respond_to do |format|
-      format.html {
-        @presidents = @district.presidents.in_office
-        @representative = @district.representatives.in_office
-        @senators = @district.senators.in_office
-      }
-      format.js {
-        @js = true
-        render :partial => 'districts/map', :locals => {:district => @district}
-      }
-    end
+    @presidents = @district.presidents.in_office
+    @representative = @district.representatives.in_office
+    @senators = @district.senators.in_office
   end
 end
