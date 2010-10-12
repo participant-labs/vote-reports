8f00e37e2bffe3c9d00ba667c2dcc1deb644c547
Ben Woosley <ben.woosley@gmail.com>
2010-10-11 22:40:55 -0700

    More guilt

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,7 +1,7 @@
 module RacesHelper
   def race_location_link(race)
     if race.district
-      "the #{Integer(district).ordinalize rescue district} District of #{link_to race.state.full_name, race.state}".html_safe
+      "the #{Integer(race.district).ordinalize rescue race.district} District of #{link_to race.state.full_name, race.state}".html_safe
     else
       link_to race.state.full_name, race.state
     end
