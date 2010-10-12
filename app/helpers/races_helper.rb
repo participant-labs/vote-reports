621cdbaf369fff58c28f4fa87fafd00000010e98
Ben Woosley <ben.woosley@gmail.com>
2010-10-11 23:05:44 -0700

    More sophisticated race_location_link means we can show office title in the guide scores

--- app/helpers/races_helper.rb
+++ app/helpers/races_helper.rb
@@ -1,7 +1,19 @@
 module RacesHelper
   def race_location_link(race)
     if race.district
-      "the #{Integer(race.district).ordinalize rescue race.district} District of #{link_to race.state.full_name, race.state}".html_safe
+      state = link_to race.state.full_name, race.state
+      begin
+        district = Integer(race.district)
+        district_name =
+          if race.office.name == 'U.S. House'
+            link_to "#{district.ordinalize} Congressional District", race.state.congressional_districts.find_by_district(district)
+          else
+            "#{district.ordinalize} #{race.office.name} District"
+          end
+        "the #{district_name} of #{state}".html_safe
+      rescue
+        "the #{race.district} #{race.office.name} District of #{state}".html_safe
+      end
     else
       link_to race.state.full_name, race.state
     end
