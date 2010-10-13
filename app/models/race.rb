867e169e94df778f03710e33aa5ec21782988a34
Ben Woosley <ben.woosley@gmail.com>
2010-10-13 02:46:15 -0700

    Switch up Race.for_districts to not screw with non-integer district names

--- app/models/race.rb
+++ app/models/race.rb
@@ -13,15 +13,15 @@ class Race < ActiveRecord::Base
       :joins => [:office, {:election_stage => :election}],
       :conditions => [
         %{elections.state_id = ? AND (
-            (offices.id = ? AND races.district_name = ?) OR
-            (offices.id = ? AND races.district_name = ?) OR
-            (offices.id IN(?) AND races.district_name = ?) OR
+            (offices.id IN(?) AND races.district_name IN(?)) OR
+            (offices.id IN(?) AND races.district_name IN(?)) OR
+            (offices.id IN(?) AND races.district_name IN(?)) OR
             (offices.id NOT IN(?))
         )},
         districts.first.state.id,
-        Office.us_house,     districts.level('federal').first.name,
-        Office.state_senate, districts.level('state_upper').first.name.to_i.to_s,
-        Office.state_lower,  districts.level('state_lower').first.name.to_i.to_s,
+        Office.us_house,     districts.level('federal').map {|d| d.name =~ /^\d*$/ ? d.name.to_i.to_s : d.name },
+        Office.state_senate, districts.level('state_upper').map {|d| d.name =~ /^\d*$/ ? d.name.to_i.to_s : d.name },
+        Office.state_lower,  districts.level('state_lower').map {|d| d.name =~ /^\d*$/ ? d.name.to_i.to_s : d.name },
         Office.districted
       ]
     }
