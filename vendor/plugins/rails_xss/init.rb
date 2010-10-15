62f0ebaa1061377762819da2b1a0870b5c37c876
Ben Woosley <ben.woosley@gmail.com>
2010-10-14 19:33:07 -0700

    Fix rails_xss version comparison fail via http://github.com/bayan/rails_xss.git

--- vendor/plugins/rails_xss/init.rb
+++ vendor/plugins/rails_xss/init.rb
@@ -1,5 +1,5 @@
 unless $gems_rake_task
-  if Rails.version <= "2.3.7"
+  if (Rails.version.split('.').map(&:to_i) <=> "2.3.7".split('.').map(&:to_i)) < 1
     $stderr.puts "rails_xss requires Rails 2.3.8 or later. Please upgrade to enable automatic HTML safety."
   else
     require 'rails_xss'
