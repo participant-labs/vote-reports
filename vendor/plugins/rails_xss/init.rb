2da0607682bc9ef1f5fca5e9eb7a8a554c813fe8
Ben Woosley <ben.woosley@gmail.com>
2010-10-14 19:29:19 -0700

    Update rails_xss

--- vendor/plugins/rails_xss/init.rb
+++ vendor/plugins/rails_xss/init.rb
@@ -1,6 +1,6 @@
 unless $gems_rake_task
-  if Rails.version <= "2.3.6"
-    $stderr.puts "rails_xss requires Rails 2.3.7 or later. Please upgrade to enable automatic HTML safety."
+  if Rails.version <= "2.3.7"
+    $stderr.puts "rails_xss requires Rails 2.3.8 or later. Please upgrade to enable automatic HTML safety."
   else
     require 'rails_xss'
   end
