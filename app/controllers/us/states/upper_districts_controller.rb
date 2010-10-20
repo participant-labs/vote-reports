63eb4361b7d52d24af3533e07cdda0a224295ae0
Ben Woosley <ben.woosley@gmail.com>
2010-10-20 13:06:57 -0700

    Fix up the state districts controller to actually find the district

--- app/controllers/us/states/upper_districts_controller.rb
+++ app/controllers/us/states/upper_districts_controller.rb
@@ -1,7 +1,7 @@
 class Us::States::UpperDistrictsController < ApplicationController
   def show
-    @state = UsState.find(params[:state_id])
-    @district = @state.districts.find_by_name_and_level(params[:id], 'state_upper')
+    @state = UsState.find(params[:state_id].upcase)
+    @district = @state.districts.state_upper.with_name(params[:id]).first
 
     @presidents = @state.presidents.in_office
     @senators = @state.senators.in_office
