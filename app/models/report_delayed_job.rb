5705fa6bb26e28c4e1832c3cb5cffe65df3e0dc9
Ben Woosley <ben.woosley@gmail.com>
2011-08-14 02:52:48 -0700

    Disambiguate Delayed::Job

--- app/models/report_delayed_job.rb
+++ app/models/report_delayed_job.rb
@@ -1,4 +1,4 @@
 class ReportDelayedJob < ActiveRecord::Base
   belongs_to :report
-  belongs_to :delayed_job, :class_name => 'Delayed::Job'
+  belongs_to :delayed_job, :class_name => '::Delayed::Job'
 end
