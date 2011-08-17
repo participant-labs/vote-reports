8543f3eec4317a7e3591afd205242ca922ef026f
Ben Woosley <ben.woosley@gmail.com>
2011-08-16 18:23:24 -0700

    Use .find's behavior rather than replicating it

--- app/controllers/reports/bill_criteria_controller.rb
+++ app/controllers/reports/bill_criteria_controller.rb
@@ -2,10 +2,7 @@ class Reports::BillCriteriaController < ApplicationController
   filter_access_to :all
 
   def autofetch
-    @report = Report.find_by_id(params[:report_id])
-    unless @report
-      raise ActiveRecord::RecordNotFound.new("No report for #{params[:report_id]}")
-    end
+    @report = Report.find(params[:report_id])
     @criteria = @report.bill_criteria.autofetch_from!(params[:source])
 
     render :partial => 'reports/bill_criteria/table', :locals => {
