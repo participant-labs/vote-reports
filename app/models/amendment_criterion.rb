04c7f66613c49e6f4e929a53e8d23a4d2a66c740
Ben Woosley <ben.woosley@gmail.com>
2011-08-25 12:38:07 -0700

    I don't think this is necessary, as it's just the id that should be passed in on creation

--- app/models/amendment_criterion.rb
+++ app/models/amendment_criterion.rb
@@ -7,8 +7,6 @@ class AmendmentCriterion < ActiveRecord::Base
   validates_presence_of :amendment, :report
   validates_uniqueness_of :amendment_id, :scope => "report_id"
 
-  accepts_nested_attributes_for :amendment
-
   scope :by_offered_on, select('DISTINCT(amendment_criteria.*), amendments.offered_on').joins(:amendment).order('amendments.offered_on DESC')
   scope :with_votes, select('DISTINCT amendment_criteria.*').joins(:amendment => :rolls)
   scope :active, with_votes.where(['rolls.roll_type IN(?)', Amendment::ROLL_PASSAGE_TYPES])
