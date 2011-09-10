3c882c699633f5ee004888cb97e9b47fc2ca90a9
Ben Woosley <ben.woosley@gmail.com>
2011-09-10 14:42:05 -0700

    Attempt to trust rvmrc programatically

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,31 +1,36 @@
-require 'auto_tagger/recipes'
+# require 'auto_tagger/recipes'
 require "bundler/capistrano"
 
 set :domain, 'votereports-app'
 
 server domain, :app, :web, :db, :primary => true
 
+set :use_sudo, false
 set :application, 'vote-reports'
 set :stages, [:staging, :production]
-set :auto_tagger_stages, stages
+# set :auto_tagger_stages, stages
 require 'capistrano/ext/multistage'
 
 $:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
-set :rvm_type, :user
 require "rvm/capistrano"                  # Load RVM's capistrano plugin.
 set :rvm_ruby_string, '1.9.2@votereports'        # Or whatever env you want it to run in.
+set :rvm_type, :user
 
+ssh_options[:forward_agent] = true
+default_run_options[:pty] = true
+set :scm, "git"
+set :repository, 'git@github.com:Empact/vote-reports.git'
 set :branch, 'rails31'
 
-before "deploy:update_code", "release_tagger:set_branch"
-after  "deploy", "release_tagger:create_tag"
-after  "deploy", "release_tagger:write_tag_to_shared"
-after  "deploy", "release_tagger:print_latest_tags"
+set :deploy_to, '/srv/vote-reports/'
+
+# before "deploy:update_code", "release_tagger:set_branch"
+# after  "deploy", "release_tagger:create_tag"
+# after  "deploy", "release_tagger:write_tag_to_shared"
+# after  "deploy", "release_tagger:print_latest_tags"
 
 set :user, 'deploy'
 
-set :deploy_via, :remote_cache
-set :copy_cache, true
 set :copy_exclude, [".git"]
 set :use_sudo, false
 
