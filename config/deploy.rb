cf43d8b33373f7887e1d68d4693f0427b669f24c
Ben Woosley <ben.woosley@gmail.com>
2011-09-10 15:13:43 -0700

    Add db config symlinking to deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,6 +42,14 @@ namespace :rvm do
 end
 after "deploy:update_code", "rvm:trust_rvmrc"
 
+namespace :deploy do
+  desc "Symlinks the database.yml"
+  task :symlink_db, :roles => :app do
+    run "ln -nfs #{deploy_to}/shared/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+after 'deploy:update_code', 'deploy:symlink_db'
+
 # namespace :monit do
 #   task :reload do
 #     sudo "monit reload"
