5920def9109c1bf594328ad8b7a6df9dbdb2cd92
Ben Woosley <ben.woosley@gmail.com>
2011-12-30 18:34:20 +0100

    Update the staging environment so we can start solr under it

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -6,7 +6,7 @@ VoteReports::Application.configure do
   config.cache_classes = true
 
   # Full error reports are disabled and caching is turned on
-  config.action_controller.consider_all_requests_local = true
+  config.consider_all_requests_local = true
   config.action_controller.perform_caching             = true
   config.action_view.cache_template_loading            = true
 
