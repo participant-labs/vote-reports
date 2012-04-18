4c29006c04c3bedde4fa3048f8543d16d329dce2
Ben Woosley <ben.woosley@gmail.com>
2012-04-18 09:38:00 -0700

    Remove manual mongoid connection in initializer and scorer - if we see the closed stream error again we can resucitate it.

--- app/models/report/scorer.rb
+++ app/models/report/scorer.rb
@@ -16,15 +16,6 @@ class Report
     end
 
     def perform
-      require 'mongo_mapper'
-
-      config = YAML.load_file(Rails.root.join('config', 'mongo.yml'))
-      MongoMapper.setup(config, Rails.env, {
-        :logger    => Rails.logger,
-        passenger: true,
-      })
-      MongoMapper.database = config[Rails.env]['database']
-
       rescue_and_reraise do
         @bases = {}
         ActiveRecord::Base.transaction do
--- config/initializers/mongo.rb
+++
@@ -1,8 +0,0 @@
-require 'mongo_mapper'
-
-config = YAML.load_file(Rails.root.join('config', 'mongo.yml'))
-MongoMapper.setup(config, Rails.env, {
-  :logger    => Rails.logger,
-  passenger: true,
-})
-MongoMapper.database = config[Rails.env]['database']
