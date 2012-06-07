1e5a0626b3e4d0906e36f0138d5a08e1dbb3b990
Ben Woosley <ben.woosley@gmail.com>
2012-06-06 17:05:07 -0700

    Swich interest_grous/claiming.feature to rspec

---
+++ spec/controllers/interest_groups/claims_controller_spec.rb
@@ -0,0 +1,14 @@
+require 'spec_helper'
+
+describe InterestGroups::ClaimsController do
+  let(:interest_group) { create_interest_group }
+
+  describe 'GET new' do
+    render_views
+
+    it 'succeeds' do
+      get :new, interest_group_id: interest_group
+      response.body.should include('just send us an email send us an email at')
+    end
+  end
+end
