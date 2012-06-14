ab21cc494697303e8f4e690d3a53e504c1e84523
Ben Woosley <ben.woosley@gmail.com>
2012-06-13 17:31:05 -0700

    Fix that the congressional_district_name helper wasn't being escaped properly.

--- app/helpers/us/congressional_districts_helper.rb
+++ app/helpers/us/congressional_districts_helper.rb
@@ -2,6 +2,6 @@ module Us::CongressionalDistrictsHelper
   def congressional_district_name(district)
     [possesive(state_name(district.state)),
      link_to_unless_current("#{district.which.downcase} congressional district", congressional_district_path(district)),
-    ].join(' '.html_safe)
+    ].join(' ').html_safe
   end
 end
