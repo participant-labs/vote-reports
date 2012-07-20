802294057c447636738f128acc48bcd35e77a0eb
Ben Woosley <ben.woosley@gmail.com>
2012-07-20 14:41:51 -0700

    Mock out UserSession.find in the Rpx specs so we don't violate our net connection in specs prohibition.

--- spec/controllers/users/rpx_identities_controller_spec.rb
+++ spec/controllers/users/rpx_identities_controller_spec.rb
@@ -6,6 +6,7 @@ describe Users::RpxIdentitiesController do
   describe "POST #create" do
     describe "on adding a registration to an existing user" do
       def create_post(user)
+        mock(UserSession).find { UserSession.create(user) }
         post :create, user_id: user.to_param,
           "token"=>"2a6e5bb00cd01b94752c26e55bbde78242e1514b",
           "authenticity_token"=>"WJhfd6WXN6DG9ujdhK9YG8pFK5GSPZJfRE43EedI+EQ=",
@@ -26,53 +27,53 @@ describe Users::RpxIdentitiesController do
           "stat" => "ok"
       end
 
-      context "when someone else is attempting to add" do
-        before do
-          @current = create_user
-          login(@current)
-          @modified = create_user
-          create_post(@modified)
+      context 'as a user' do
+        let(:user) { create_user }
+        before { login user }
+
+        context "when adding to someone else" do
+          let(:modified)  { create_user }
+
+          it "should redirect" do
+            create_post(modified)
+            response.should redirect_to(user_path(modified))
+          end
+
+          it "should not create an rpx identity" do
+            expect {
+              create_post(modified)
+            }.to_not change(RPXIdentifier, :count)
+          end
         end
 
-        it "should redirect" do
-          response.should redirect_to(user_path(@modified))
-        end
+        context "when I'm adding to myself" do
+          before do
+            @identifier = "https://www.google.com/accounts/o8/id?id=AItOawk9vs1fKTQ-PEoono_hovx95Nt5qWG6LUk"
+          end
 
-        it "should not create an rpx identity" do
-          RPXIdentifier.find_by_identifier(@identifier).should == nil
-          User.find_by_rpx_identifier(@identifier).should == nil
-        end
-      end
+          it "should redirect to user page" do
+            create_post(user)
+            response.should redirect_to(user_path(user))
+          end
 
-      context "when I'm adding to myself" do
-        before do
-          @current = create_user
-          login(@current)
-          @identifier = "https://www.google.com/accounts/o8/id?id=AItOawk9vs1fKTQ-PEoono_hovx95Nt5qWG6LUk"
-        end
+          it "should notify of success" do
+            create_post(user)
+            flash[:notice].should == "Successfully added login to this account."
+          end
 
-        it "should redirect to user page" do
-          create_post(@current)
-          response.should redirect_to(user_path(@current))
-        end
+          it "should create an RPXIdentifier" do
+            pending "Tokens are time-sensitive and I don't know how to generate them properly"
+            lambda {
+              create_post(user)
+            }.should change(RPXIdentifier, :count).by(1)
+          end
 
-        it "should notify of success" do
-          create_post(@current)
-          flash[:notice].should == "Successfully added login to this account."
-        end
-
-        it "should create an RPXIdentifier" do
-          pending "Tokens are time-sensitive and I don't know how to generate them properly"
-          lambda {
-            create_post(@current)
-          }.should change(RPXIdentifier, :count).by(1)
-        end
-
-        it "should create an rpx_identifier record" do
-          pending "Tokens are time-sensitive and I don't know how to generate them properly"
-          create_post(@current)
-          RPXIdentifier.find_by_identifier(@identifier).user.should == @current
-          User.find_by_rpx_identifier(@identifier).should == @current
+          it "should create an rpx_identifier record" do
+            pending "Tokens are time-sensitive and I don't know how to generate them properly"
+            create_post(user)
+            RPXIdentifier.find_by_identifier(@identifier).user.should == user
+            User.find_by_rpx_identifier(@identifier).should == user
+          end
         end
       end
     end
