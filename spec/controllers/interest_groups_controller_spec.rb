5ee036eb0014c487feca88f3aaa7d6c3f2d4bab8
Ben Woosley <ben.woosley@gmail.com>
2012-09-10 23:10:13 -0700

    Be somewhat more direct in our interest_group_controller_spec setup.

--- spec/controllers/interest_groups_controller_spec.rb
+++ spec/controllers/interest_groups_controller_spec.rb
@@ -2,20 +2,18 @@ require 'spec_helper'
 
 describe InterestGroupsController do
   describe 'GET index' do
-    let(:with_many_interest_groups) {
+    before do
       35.times { create_interest_group }
-    }
+    end
 
     context 'without arguments' do
       it 'shows the first page of interest groups' do
-        with_many_interest_groups
         get :index
         assigns[:interest_groups].should == InterestGroup.order('name').page(1)
       end
     end
     context 'with a page' do
       it 'shows that page of interest groups' do
-        with_many_interest_groups
         get :index, page: 2
         assigns[:interest_groups].should == InterestGroup.order('name').page(2)
       end
