38036e31190d545dff950629caa3acea2817cd23
Ben Woosley <ben.woosley@gmail.com>
2012-09-17 23:03:00 -0700

    Override logout to not mess with the cookies, which isn't legit under capybara

--- features/support/authlogic.rb
+++ features/support/authlogic.rb
@@ -1,5 +1,12 @@
 require Rails.root.join('spec/support/authlogic')
 
+# Override logout to not mess with the cookies, which isn't legit under capybara
+module Authlogic::Test
+  def logout
+    @current_user = nil
+  end
+end
+
 Before do
   activate_authlogic
 end
