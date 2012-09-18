e901ecd05d0fb50520e4f9f3c47389a52657f2c3
Ben Woosley <ben.woosley@gmail.com>
2012-09-18 02:25:03 -0700

    Rework BillCritrion scope selects to avoid a repeated distinct exception.

--- app/models/bill_criterion.rb
+++ app/models/bill_criterion.rb
@@ -9,10 +9,10 @@ class BillCriterion < ActiveRecord::Base
 
   accepts_nested_attributes_for :bill
 
-  scope :by_introduced_on, select('DISTINCT(bill_criteria.*), bills.introduced_on').joins(:bill).order('bills.introduced_on DESC')
+  scope :by_introduced_on, select(['DISTINCT(bill_criteria.*)', 'bills.introduced_on']).joins(:bill).order('bills.introduced_on DESC')
 
   # see also: Roll.on_bill_passage
-  scope :active, select('DISTINCT bill_criteria.*').joins([
+  scope :active, select('DISTINCT(bill_criteria.*)').joins([
       %{LEFT OUTER JOIN "bills" active_bills ON bill_criteria.bill_id = active_bills.id},
       %{LEFT OUTER JOIN "rolls" active_rolls ON active_rolls.subject_id = active_bills.id},
       %{LEFT OUTER JOIN "cosponsorships" active_cosponsorships ON active_cosponsorships.bill_id = active_bills.id}
