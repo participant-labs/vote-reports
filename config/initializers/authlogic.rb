489f08b06617403f1445f5d5bc1b65d1c01e9657
Ben Woosley <ben.woosley@gmail.com>
2014-10-12 12:47:22 -0700

    Remove defunct authlogic monkeypatch.

--- config/initializers/authlogic.rb
+++ config/initializers/authlogic.rb
@@ -11,31 +11,4 @@ module Authlogic
       end
     end
   end
-
-  module Session # :nodoc:
-    module ValidationWhichDoesntBlowUp
-      def valid?
-        errors.clear
-        self.attempted_record = nil
-
-        before_validation
-        new_session? ? before_validation_on_create : before_validation_on_update
-        validate
-        ensure_authentication_attempted
-
-        if errors.size == 0
-          new_session? ? after_validation_on_create : after_validation_on_update
-          after_validation
-        end
-
-        errors.size == 0
-      end
-    end
-
-    # This is the base class Authlogic, where all modules are included. For information on functiionality see the various
-    # sub modules.
-    class Base
-      include ValidationWhichDoesntBlowUp
-    end
-  end
 end
